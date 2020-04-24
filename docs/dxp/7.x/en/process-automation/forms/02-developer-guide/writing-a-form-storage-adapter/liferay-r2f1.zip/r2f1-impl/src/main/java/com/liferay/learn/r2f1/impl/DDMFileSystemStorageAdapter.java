17ddc52b1f46b6112574e7fd12c32d926d92ecb9
Russell Bohl <russell.bohl@liferay.com>
2020-04-24 18:21:24 -0400

    LRDOCS-7447 file-system example code

---
+++ docs/dxp/7.x/en/process-automation/forms/02-developer-guide/writing-a-form-storage-adapter/liferay-r2f1.zip/r2f1-impl/src/main/java/com/liferay/learn/r2f1/impl/DDMFileSystemStorageAdapter.java
@@ -0,0 +1,238 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.learn.r2f1.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.dynamic.data.mapping.exception.StorageException;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializerDeserializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializerDeserializeResponse;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesDeserializerTracker;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializer;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializerSerializeRequest;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializerSerializeResponse;
+import com.liferay.dynamic.data.mapping.io.DDMFormValuesSerializerTracker;
+import com.liferay.dynamic.data.mapping.model.DDMContent;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.service.DDMContentLocalService;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageAdapter;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageAdapterDeleteRequest;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageAdapterDeleteResponse;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageAdapterGetRequest;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageAdapterGetResponse;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageAdapterSaveRequest;
+import com.liferay.dynamic.data.mapping.storage.DDMStorageAdapterSaveResponse;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.FileUtil;
+
+/**
+ * @author Russell Bohl
+ */
+@Component(immediate = true, property = "ddm.storage.adapter.type=file-system", service = DDMStorageAdapter.class)
+public class DDMFileSystemStorageAdapter implements DDMStorageAdapter {
+
+	private static final String _PATHNAME = "/home/russell/liferay-bundles/master-dxp_03172020/form-records";
+
+	@Override
+	public DDMStorageAdapterDeleteResponse delete(DDMStorageAdapterDeleteRequest ddmStorageAdapterDeleteRequest)
+			throws StorageException {
+
+		long fileId = ddmStorageAdapterDeleteRequest.getPrimaryKey();
+		deleteFile(fileId);
+
+		try {
+			ddmContentLocalService.deleteDDMContent(ddmStorageAdapterDeleteRequest.getPrimaryKey());
+
+			DDMStorageAdapterDeleteResponse.Builder builder = DDMStorageAdapterDeleteResponse.Builder.newBuilder();
+
+			return builder.build();
+		} catch (Exception e) {
+			throw new StorageException(e);
+		}
+	}
+
+	public DDMStorageAdapterGetResponse get(DDMStorageAdapterGetRequest ddmStorageAdapterGetRequest)
+			throws StorageException {
+		
+		// since this example stroes in the liferay database AND on the file system,
+		// we might not need to read from the file system. what do you think?
+
+		try {
+            long fileId = ddmStorageAdapterGetRequest.getPrimaryKey();
+            getFile(fileId);
+
+			DDMContent ddmContent = ddmContentLocalService.getContent(ddmStorageAdapterGetRequest.getPrimaryKey());
+
+			DDMFormValues ddmFormValues = deserialize(ddmContent.getData(), ddmStorageAdapterGetRequest.getDDMForm());
+
+			DDMStorageAdapterGetResponse.Builder builder = DDMStorageAdapterGetResponse.Builder
+					.newBuilder(ddmFormValues);
+
+			return builder.build();
+		} catch (Exception e) {
+			throw new StorageException(e);
+		}
+	}
+
+	@Override
+	public DDMStorageAdapterSaveResponse save(DDMStorageAdapterSaveRequest ddmStorageAdapterSaveRequest)
+			throws StorageException {
+
+		try {
+			if (ddmStorageAdapterSaveRequest.isInsert()) {
+				return insert(ddmStorageAdapterSaveRequest);
+			}
+		} catch (Exception e) {
+		}
+
+		return update(ddmStorageAdapterSaveRequest);
+	}
+
+	private DDMStorageAdapterSaveResponse insert(DDMStorageAdapterSaveRequest ddmStorageAdapterSaveRequest)
+			throws PortalException, StorageException {
+
+		DDMFormValues ddmFormValues = ddmStorageAdapterSaveRequest.getDDMFormValues();
+
+		try {
+			ServiceContext serviceContext = new ServiceContext();
+
+			serviceContext.setScopeGroupId(ddmStorageAdapterSaveRequest.getScopeGroupId());
+			serviceContext.setUserId(ddmStorageAdapterSaveRequest.getUserId());
+			serviceContext.setUuid(ddmStorageAdapterSaveRequest.getUuid());
+
+			DDMContent ddmContent = ddmContentLocalService.addContent(ddmStorageAdapterSaveRequest.getUserId(),
+					ddmStorageAdapterSaveRequest.getScopeGroupId(), ddmStorageAdapterSaveRequest.getClassName(), null,
+					serialize(ddmFormValues), serviceContext);
+
+			DDMStorageAdapterSaveResponse.Builder builder = DDMStorageAdapterSaveResponse.Builder
+					.newBuilder(ddmContent.getPrimaryKey());
+
+			DDMStorageAdapterSaveResponse ddmStorageAdapterSaveResponse = builder.build();
+
+			long storageId = ddmStorageAdapterSaveRequest.getPrimaryKey();
+			long fileId = ddmStorageAdapterSaveResponse.getPrimaryKey();
+			saveFile(fileId, ddmFormValues, storageId);
+
+			return ddmStorageAdapterSaveResponse;
+
+		} catch (Exception e) {
+			throw new StorageException(e);
+		}
+
+	}
+
+	private DDMStorageAdapterSaveResponse update(DDMStorageAdapterSaveRequest ddmStorageAdapterSaveRequest)
+			throws StorageException {
+
+		DDMFormValues ddmFormValues = ddmStorageAdapterSaveRequest.getDDMFormValues();
+		long storageId = ddmStorageAdapterSaveRequest.getPrimaryKey();
+
+		try {
+			DDMContent ddmContent = ddmContentLocalService.getContent(ddmStorageAdapterSaveRequest.getPrimaryKey());
+
+			ddmContent.setModifiedDate(new Date());
+
+			ddmContent.setData(serialize(ddmStorageAdapterSaveRequest.getDDMFormValues()));
+
+			ddmContentLocalService.updateContent(ddmContent.getPrimaryKey(), ddmContent.getName(),
+					ddmContent.getDescription(), ddmContent.getData(), new ServiceContext());
+
+			DDMStorageAdapterSaveResponse.Builder builder = DDMStorageAdapterSaveResponse.Builder
+					.newBuilder(ddmContent.getPrimaryKey());
+
+			DDMStorageAdapterSaveResponse ddmStorageAdapterSaveResponse = builder.build();
+
+			long fileId = ddmStorageAdapterSaveResponse.getPrimaryKey();
+			saveFile(fileId, ddmFormValues, storageId);
+
+			return ddmStorageAdapterSaveResponse;
+		} catch (Exception e) {
+			throw new StorageException(e);
+		}
+	}
+
+	private void saveFile(long fileId, DDMFormValues formValues, long storageId) throws IOException {
+
+		try {
+			String serializedDDMFormValues = serialize(formValues);
+			File abstractFile = new File(String.valueOf(fileId));
+			FileUtil.write(_PATHNAME, abstractFile.getName(), serializedDDMFormValues);
+			System.out.println("File written to" + _PATHNAME);
+		} catch (IOException e) {
+			throw new IOException(e);
+		}
+	}
+
+	private void deleteFile(long fileId) {
+
+		File file = new File(_PATHNAME + "/" + fileId);
+		file.delete();
+
+	}
+
+	private void getFile(long fileId) throws IOException {
+		try {
+		System.out.println("Reading the file named:" + fileId + "\n" +
+				"The file contents: " + FileUtil.read(_PATHNAME + "/" + fileId));
+		}
+		catch(IOException e) {
+			throw new IOException(e);
+		}
+	}
+
+	private DDMFormValues deserialize(String content, DDMForm ddmForm) {
+		DDMFormValuesDeserializer ddmFormValuesDeserializer = ddmFormValuesDeserializerTracker
+				.getDDMFormValuesDeserializer("json");
+
+		DDMFormValuesDeserializerDeserializeRequest.Builder builder = DDMFormValuesDeserializerDeserializeRequest.Builder
+				.newBuilder(content, ddmForm);
+
+		DDMFormValuesDeserializerDeserializeResponse ddmFormValuesDeserializerDeserializeResponse = ddmFormValuesDeserializer
+				.deserialize(builder.build());
+
+		return ddmFormValuesDeserializerDeserializeResponse.getDDMFormValues();
+	}
+
+	private String serialize(DDMFormValues ddmFormValues) {
+		DDMFormValuesSerializer ddmFormValuesSerializer = ddmFormValuesSerializerTracker
+				.getDDMFormValuesSerializer("json");
+
+		DDMFormValuesSerializerSerializeRequest.Builder builder = DDMFormValuesSerializerSerializeRequest.Builder
+				.newBuilder(ddmFormValues);
+
+		DDMFormValuesSerializerSerializeResponse ddmFormValuesSerializerSerializeResponse = ddmFormValuesSerializer
+				.serialize(builder.build());
+
+		return ddmFormValuesSerializerSerializeResponse.getContent();
+	}
+	
+	@Reference
+	private DDMContentLocalService ddmContentLocalService;
+
+	@Reference
+	private DDMFormValuesDeserializerTracker ddmFormValuesDeserializerTracker;
+
+	@Reference
+	private DDMFormValuesSerializerTracker ddmFormValuesSerializerTracker;
+
+}
