460a2e27054d05c6303461403f80109813688ccf
Brian Chan <brian.chan@liferay.com>
2020-06-26 08:59:50 -0700

    LRDOCS-8010 Rich, we have to do it this way, or when I run update_examples.sh, it'll change all of commerce to the dxp product

--- docs/update_examples.sh
+++ docs/update_examples.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-for zip_dir_name in `find . -name "liferay-*.zip" -type d`
-do
-	cp -fr _template/* ${zip_dir_name}
+function copy_template {
+	for zip_dir_name in `find . -name "liferay-*.zip" -type d`
+	do
+		cp -fr _template/* ${zip_dir_name}
 
-	#pushd ${dir_name}
+		local liferay_workspace_product=portal-7.3-ga3
 
-	#./gradlew classes formatSource
+		if [[ ${zip_dir_name} == ./commerce/** ]]
+		then
+			liferay_workspace_product=commerce-2.0.7-7.2
+		fi
 
-	#popd
-done
+		echo -ne "liferay.workspace.product=${liferay_workspace_product}" > ${zip_dir_name}/gradle.properties
+
+		#pushd ${dir_name}
+
+		#./gradlew classes formatSource
+
+		#popd
+	done
+}
+
+function main {
+	copy_template
+}
+
+main
