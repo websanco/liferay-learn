41eb8f856b2bb9efd2fef8bda455dbfa361aac59
Russell Bohl <russell.bohl@liferay.com>
2020-06-30 14:38:15 -0400

    get the site tokens in a function

--- site/build_site.sh
+++ site/build_site.sh
@@ -16,6 +16,15 @@ function check_utils {
 	done
 }
 
+#
+# Extract the product, version, and language from paths.
+#
+function get_site_tokens {
+	product=$(echo "${path}" | cut -f3 -d'/')
+	version=$(echo "${path}" | cut -f4 -d'/')
+	language=$(echo "${path}" | cut -f5 -d'/')
+}
+
 function generate_sphinx_input {
 	rm -fr build
 
@@ -27,44 +36,40 @@ function generate_sphinx_input {
 
 	cd ../site
 
-	for path in `find ../docs -maxdepth 4 -mindepth 4 -type f -name "contents.rst" -printf "%h\n" `
+	for path in `find ../docs -maxdepth 4 -mindepth 4 -type f -name "contents.rst" -printf "%h\n"`
 	do
-		# removes the ../docs/ from the found paths, returning "product/version/language"
-		site_path=$(echo "${path}" | cut -f3- -d'/')
+		get_site_tokens
 
-		mkdir -p build/input/${site_path}/docs
+		mkdir -p build/input/${product}/${version}/${language}/docs
 
-		cp -R docs/* build/input/${site_path}
+		cp -R docs/* build/input/${product}/${version}/${language}
 
-		cp -R ../docs/${site_path}/* build/input/${site_path}
+		cp -R ../docs/${product}/${version}/${language}/* build/input/${product}/${version}/${language}
 	done
 
 	rsync -a homepage/* build/input/homepage --exclude={'*.json','node_modules'}
-
 }
 
 function generate_static_html {
 	for path in `find build/input -maxdepth 4 -mindepth 4 -type f -name "contents.rst" -printf "%h\n" `
 	do
-		# removes the build/input/ from the found paths, returning "product/version/language"
-		site_path=$(echo "${path}" | cut -f3- -d'/')
+		get_site_tokens
 
-		echo "Generating static html for $site_path"
+		echo "Generating static html for ${product} ${version} ${language}"
 
 		#
-		# Use Sphinx to generate static HTML for each
-		#   product/version/language. The Homepage content is built
-		#   separately at the end of the function.
+		# Use Sphinx to generate static HTML for each product/version/language.
 		#
-		sphinx-build -M html "build/input/${site_path}" "build/output/${site_path}"
 
-		mv build/output/${site_path}/html/* build/output/${site_path}
+		sphinx-build -M html "build/input/${product}/${version}/${language}" "build/output/${product}/${version}/${language}"
+
+		mv build/output/${product}/${version}/${language}/html/* build/output/${product}/${version}/${language}
 
 		#
 		# Fix broken links.
 		#
 
-		for html_file_name in `find build/output/${site_path} -name *.html -type f`
+		for html_file_name in `find build/output/${product}/${version}/${language} -name *.html -type f`
 		do
 			sed -i 's/.md"/.html"/g' ${html_file_name}
 			sed -i 's/.md#/.html#/g' ${html_file_name}
@@ -76,7 +81,7 @@ function generate_static_html {
 		# Rename README.html to index.html.
 		#
 
-		for readme_file_name in `find build/output/${site_path} -name *README.html -type f`
+		for readme_file_name in `find build/output/${product}/${version}/${language} -name *README.html -type f`
 		do
 			mv ${readme_file_name} $(dirname ${readme_file_name})/index.html
 		done
@@ -85,13 +90,13 @@ function generate_static_html {
 		# Update search references for README.html to index.html.
 		#
 
-		sed -i 's/README"/index"/g' build/output/${site_path}/searchindex.js
+		sed -i 's/README"/index"/g' build/output/${product}/${version}/${language}/searchindex.js
 
 		#
 		# Make ZIP files.
 		#
 
-		for zip_dir_name in `find build/input/${site_path} -name *.zip -type d`
+		for zip_dir_name in `find build/input/${product}/${version}/${language} -name *.zip -type d`
 		do
 			pushd ${zip_dir_name}
 
@@ -109,6 +114,10 @@ function generate_static_html {
 			mv ${zip_dir_name}/${zip_file_name} ${output_dir_name}
 		done
 	done
+
+	#
+	# Use Sphinx to build the Homepage content separately.
+	#
 
 	sphinx-build -M html build/input/homepage build/output/homepage
 
