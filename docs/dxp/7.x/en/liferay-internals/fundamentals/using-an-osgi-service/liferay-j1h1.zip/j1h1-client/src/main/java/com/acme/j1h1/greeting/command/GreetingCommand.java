29126c1b9e7f3e963382e1c9dcd0a1c8e1956aa8
James Hinkey <james.hinkey@liferay.com>
2020-09-02 13:18:22 -0400

    LRDOCS-8286 code for "Using an OSGi Service"

---
+++ docs/dxp/7.x/en/liferay-internals/fundamentals/using-an-osgi-service/liferay-j1h1.zip/j1h1-client/src/main/java/com/acme/j1h1/greeting/command/GreetingCommand.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.acme.j1h1.greeting.command;
+
+import com.acme.j1h1.greeting.Greeting;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author James Hinkey
+ */
+@Component(
+	property = {"osgi.command.function=greet", "osgi.command.scope=greet"},
+	service = Object.class
+)
+public class GreetingCommand {
+
+	public void greet(String name) {
+		Greeting greeting = _greeting;
+
+		greeting.greet(name);
+	}
+
+	@Reference
+	private Greeting _greeting;
+
+}
---
+++ docs/dxp/7.x/en/liferay-internals/fundamentals/using-an-osgi-service/liferay-j1h1.zip/j1h1-impl/src/main/java/com/acme/j1h1/internal/greeting/J1H1Greeting.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.acme.j1h1.internal.greeting;
+
+import com.acme.j1h1.greeting.Greeting;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author James Hinkey
+ */
+@Component(service = Greeting.class)
+public class J1H1Greeting implements Greeting {
+
+	@Override
+	public void greet(String name) {
+		System.out.println("Hello " + name + "!");
+	}
+
+}
