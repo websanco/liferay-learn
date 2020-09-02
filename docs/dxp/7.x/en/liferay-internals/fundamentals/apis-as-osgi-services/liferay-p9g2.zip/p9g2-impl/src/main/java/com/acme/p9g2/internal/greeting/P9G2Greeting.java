6320e710b41484d7b4d8d92cd021d5643f4560a8
James Hinkey <james.hinkey@liferay.com>
2020-09-02 13:43:42 -0400

    LRDOCS-7981 code for "APIs as OSGi Services"

---
+++ docs/dxp/7.x/en/liferay-internals/fundamentals/apis-as-osgi-services/liferay-p9g2.zip/p9g2-impl/src/main/java/com/acme/p9g2/internal/greeting/P9G2Greeting.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.acme.p9g2.internal.greeting;
+
+import com.acme.p9g2.greeting.Greeting;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author James Hinkey
+ */
+@Component(service = Greeting.class)
+public class P9G2Greeting implements Greeting {
+
+	@Override
+	public void greet(String name) {
+		System.out.println("Hello " + name + "!");
+	}
+
+}
