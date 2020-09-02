a13a248b709cfc3f2ada4bf26b71814609399c56
Alec-Shay <Alec.Shay@liferay.com>
2020-09-02 11:28:43 -0700

    gradlew formatSource

--- docs/dxp/7.x/en/headless-delivery/content-delivery-apis/rest-builder/implementing-a-new-api-with-rest-builder/liferay-r3b2.zip/r3b2-impl/src/main/java/com/acme/r3b2/journal/article/expansion/internal/resource/v1_0/ArticleResourceImpl.java
+++ docs/dxp/7.x/en/headless-delivery/content-delivery-apis/rest-builder/implementing-a-new-api-with-rest-builder/liferay-r3b2.zip/r3b2-impl/src/main/java/com/acme/r3b2/journal/article/expansion/internal/resource/v1_0/ArticleResourceImpl.java
@@ -6,10 +6,10 @@ import com.acme.r3b2.journal.article.expansion.resource.v1_0.ArticleResource;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.service.JournalArticleLocalServiceUtil;
 
+import java.util.List;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ServiceScope;
-
-import java.util.List;
 
 /**
  * @author Liferay
@@ -20,23 +20,25 @@ import java.util.List;
 )
 public class ArticleResourceImpl extends BaseArticleResourceImpl {
 
-    @Override
-    public Article getFirstArticleByUser(Integer userId) {
-        List<JournalArticle> journalArticles = JournalArticleLocalServiceUtil.getArticles();
+	@Override
+	public Article getFirstArticleByUser(Integer userId) {
+		List<JournalArticle> journalArticles =
+			JournalArticleLocalServiceUtil.getArticles();
 
-        for (JournalArticle journalArticle : journalArticles) {
-            if (journalArticle.getUserId() == userId.longValue()) {
-                Article article = new Article();
+		for (JournalArticle journalArticle : journalArticles) {
+			if (journalArticle.getUserId() == userId.longValue()) {
+				Article article = new Article();
 
-                article.setUserId((int)journalArticle.getUserId());
-                article.setId(journalArticle.getArticleId());
-                article.setTitle(journalArticle.getTitle());
-                article.setContent(journalArticle.getContent());
+				article.setUserId((int)journalArticle.getUserId());
+				article.setId(journalArticle.getArticleId());
+				article.setTitle(journalArticle.getTitle());
+				article.setContent(journalArticle.getContent());
 
-                return article;
-            }
-        }
+				return article;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
+
 }
