8f188dc8411a859d1f2d5ba9a0d08314e8f00a14
Russell Bohl <russell.bohl@liferay.com>
2020-09-17 16:29:05 -0400

    fix a couple mistakes

--- site/build_site.sh
+++ site/build_site.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 readonly CURRENT_DIR_NAME=$(dirname "$0")
 
 readonly LIFERAY_LEARN_DXP_VERSION_TOKEN=\\[\$LIFERAY_LEARN_DXP_VERSION\$\\]
-readonly LIFERAY_LEARN_DXP_VERSION_VALUE=7.3.0-ga1
+readonly LIFERAY_LEARN_DXP_VERSION_VALUE=7.3.10-ga1
 readonly LIFERAY_LEARN_DXP_VERSION_VALUE_CE=7.3.4-ga5
 
 function activate_venv {
@@ -120,7 +120,7 @@ function generate_sphinx_input {
 	do
 		sed -i "s/\(^\s*docker.*liferay\/dxp\:\)${LIFERAY_LEARN_DXP_VERSION_TOKEN}/\1${LIFERAY_LEARN_DXP_VERSION_VALUE}/g" ${md_file_name}
 		sed -i "s/\(^\s*docker.*liferay\/portal\:\)${LIFERAY_LEARN_DXP_VERSION_TOKEN}/\1${LIFERAY_LEARN_DXP_VERSION_VALUE_CE}/g" ${md_file_name}
-		sed -i "s/${LIFERAY_LEARN_DXP_VERSION_TOKEN}/${LIFERAY_LEARN_DXP_VERSION_VALUE}/g" ${md_file_name}
+		sed -i "s/${LIFERAY_LEARN_DXP_VERSION_TOKEN}/${LIFERAY_LEARN_DXP_VERSION_VALUE_CE}/g" ${md_file_name}
 	done
 }
 
