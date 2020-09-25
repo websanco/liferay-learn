6df5acbcd344ba02de59e844498133f440ecf672
Alec-Shay <Alec.Shay@liferay.com>
2020-09-25 15:13:26 -0700

    Fix bnd.bnd module name and widget display name

--- docs/dxp/7.x/en/developing-applications/data-frameworks/implementing-an-item-selector/liferay-f5d5.zip/f5d5-web/src/main/java/com/acme/f5d5/web/internal/f5d5/F5D5JSPPortlet.java
+++ docs/dxp/7.x/en/developing-applications/data-frameworks/implementing-an-item-selector/liferay-f5d5.zip/f5d5-web/src/main/java/com/acme/f5d5/web/internal/f5d5/F5D5JSPPortlet.java
@@ -32,7 +32,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=Item Selector Example",
+		"javax.portlet.display-name=F5D5",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.resource-bundle=content.Language",
