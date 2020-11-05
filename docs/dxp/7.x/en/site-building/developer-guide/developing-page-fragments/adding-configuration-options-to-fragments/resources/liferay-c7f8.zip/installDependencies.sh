61533654e5c134632851e5f1e1220cfedda53f78
James Hinkey <james.hinkey@liferay.com>
2020-11-05 14:49:05 -0500

    c7f8 scripts to set environment and install dependencies

---
+++ docs/dxp/7.x/en/site-building/developer-guide/developing-page-fragments/adding-configuration-options-to-fragments/resources/liferay-c7f8.zip/installDependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+NPM_CONFIG_PREFIX=~/.npm-global npm install -g generator-liferay-fragments generator-liferay-js generator-liferay-theme yo
---
+++ docs/dxp/7.x/en/site-building/developer-guide/developing-page-fragments/adding-configuration-options-to-fragments/resources/liferay-c7f8.zip/setenv.sh
@@ -0,0 +1,5 @@
+NPM_CONFIG_PREFIX=~/.npm-global
+export NPM_CONFIG_PREFIX
+
+PATH=$PATH:~/.npm-global/bin
+export PATH
