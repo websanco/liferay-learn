8708cc194a3f9cb09cf198a09aadff44647c8e98
Brian Chan <brian.chan@liferay.com>
2020-11-06 12:26:54 -0800

    c7f8 tabs

--- docs/dxp/7.x/en/site-building/developer-guide/developing-page-fragments/adding-configuration-options-to-fragments/resources/liferay-c7f8.zip/setup_tutorial.sh
+++ docs/dxp/7.x/en/site-building/developer-guide/developing-page-fragments/adding-configuration-options-to-fragments/resources/liferay-c7f8.zip/setup_tutorial.sh
@@ -2,32 +2,32 @@
 
 if [ -z "${NPM_CONFIG_PREFIX}" ]
 then
-    echo "The environment variable NPM_CONFIG_PREFIX is not set and needs to point to a folder for storing Node packages. Run this command:
+	echo "The environment variable NPM_CONFIG_PREFIX is not set and needs to point to a folder for storing Node packages. Run this command:
 
-    export NPM_CONFIG_PREFIX=~/.npm-global"
+	export NPM_CONFIG_PREFIX=~/.npm-global"
 
-    exit
+	exit
 fi
 
 if [[ "${PATH}" != *"${NPM_CONFIG_PREFIX}/bin"* ]]
 then
-    echo "The environment variable PATH does not include ${NPM_CONFIG_PREFIX}/bin. Add ${NPM_CONFIG_PREFIX}/bin to your PATH. Run this command:
+	echo "The environment variable PATH does not include ${NPM_CONFIG_PREFIX}/bin. Add ${NPM_CONFIG_PREFIX}/bin to your PATH. Run this command:
 
-    export PATH=\${PATH}:\${NPM_CONFIG_PREFIX}/bin"
+	export PATH=\${PATH}:\${NPM_CONFIG_PREFIX}/bin"
 
-    exit
+	exit
 fi
 
 if  [ ! -d "${NPM_CONFIG_PREFIX}/lib/node_modules/generator-liferay-fragments" ] ||
-    [ ! -d "${NPM_CONFIG_PREFIX}/lib/node_modules/generator-liferay-js" ] ||
-    [ ! -d "${NPM_CONFIG_PREFIX}/lib/node_modules/generator-liferay-theme" ] ||
-    [ ! -d "${NPM_CONFIG_PREFIX}/lib/node_modules/yo" ]
+	[ ! -d "${NPM_CONFIG_PREFIX}/lib/node_modules/generator-liferay-js" ] ||
+	[ ! -d "${NPM_CONFIG_PREFIX}/lib/node_modules/generator-liferay-theme" ] ||
+	[ ! -d "${NPM_CONFIG_PREFIX}/lib/node_modules/yo" ]
 then
-    echo "A tutorial dependency is missing. Run this command:
+	echo "A tutorial dependency is missing. Run this command:
 
-    npm install -g generator-liferay-fragments generator-liferay-js generator-liferay-theme yo"
+	npm install -g generator-liferay-fragments generator-liferay-js generator-liferay-theme yo"
 
-    exit
+	exit
 fi
 
 echo "Your environment is ready for the tutorial."
