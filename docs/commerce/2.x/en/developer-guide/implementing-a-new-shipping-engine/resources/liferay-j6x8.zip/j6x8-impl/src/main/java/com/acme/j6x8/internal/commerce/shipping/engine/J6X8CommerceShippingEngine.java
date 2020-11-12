3119aacc6dd0306f6d0e373a2ffc60abf92066fc
James Hinkey <james.hinkey@liferay.com>
2020-11-12 11:41:20 -0500

    j6x8 immediate=true not needed

--- docs/commerce/2.x/en/developer-guide/implementing-a-new-shipping-engine/resources/liferay-j6x8.zip/j6x8-impl/src/main/java/com/acme/j6x8/internal/commerce/shipping/engine/J6X8CommerceShippingEngine.java
+++ docs/commerce/2.x/en/developer-guide/implementing-a-new-shipping-engine/resources/liferay-j6x8.zip/j6x8-impl/src/main/java/com/acme/j6x8/internal/commerce/shipping/engine/J6X8CommerceShippingEngine.java
@@ -31,7 +31,6 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 @Component(
-	immediate = true,
 	property = "commerce.shipping.engine.key=J6X8",
 	service = CommerceShippingEngine.class
 )
