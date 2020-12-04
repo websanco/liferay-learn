cf5380200c86eb6f56fe4b1c68b834dd7560fa45
Peter Shin <peter.shin@liferay.com>
2020-12-04 15:26:44 -0600

    LRDOCS-0 Ran "./gradlew formatPython"

--- site/docs/conf.py
+++ site/docs/conf.py
@@ -7,24 +7,29 @@ import recommonmark
 from recommonmark.transform import AutoStructify
 
 # Variables to set product name, version, and language using directory paths
-language_path = Path(os.path.dirname(__file__));
+language_path = Path(os.path.dirname(__file__))
 
-version_path = language_path.parent;
+version_path = language_path.parent
 
-product_path = version_path.parent;
+product_path = version_path.parent
 
 #
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 #
 
 author = "Liferay"
-copybutton_image_path="img/paste.svg"
+copybutton_image_path = "img/paste.svg"
 copyright = "2020, Liferay"
-extensions = ["notfound.extension", "recommonmark", "sphinx_copybutton", "sphinx_markdown_tables"]
+extensions = [
+    "notfound.extension",
+    "recommonmark",
+    "sphinx_copybutton",
+    "sphinx_markdown_tables",
+]
 html_context = {
-	"available_languages": os.listdir('..'),
-	"product_name": os.path.basename(product_path),
-	"product_version": os.path.basename(version_path)
+    "available_languages": os.listdir(".."),
+    "product_name": os.path.basename(product_path),
+    "product_version": os.path.basename(version_path),
 }
 html_css_files = ["main.min.css"]
 html_favicon = "_static/img/favicon.ico"
@@ -46,38 +51,38 @@ source_suffix = [".md", ".rst"]
 templates_path = ["_template"]
 version = "1.0"
 
+
 class WithRootSiteHTMLBuilder(StandaloneHTMLBuilder):
-	def get_doc_context(self, docname, body, metatags):
-		doc_context = super().get_doc_context(docname, body, metatags)
+    def get_doc_context(self, docname, body, metatags):
+        doc_context = super().get_doc_context(docname, body, metatags)
 
-		if docname != 'README':
-			# Set the README.md on the root level as the "fake" parent
-			# ie: docs/commerce/README.md
-			site_parent = self.get_relative_uri(docname, 'README')
+        if docname != "README":
+            # Set the README.md on the root level as the "fake" parent
+            # ie: docs/commerce/README.md
+            site_parent = self.get_relative_uri(docname, "README")
 
-			# Set the document title as site title
-			# ie: Commerce or DXP Cloud, etc
-			site_title = self.render_partial(self.env.titles['README'])['title']
+            # Set the document title as site title
+            # ie: Commerce or DXP Cloud, etc
+            site_title = self.render_partial(self.env.titles["README"])["title"]
 
-			# Add the "fake" parent to the parents object, so that "DXP", "Commerce", etc.
-			# is visible in the breadcrumbs
-			doc_context['parents'].insert(
-				0,
-				{
-					'link': site_parent,
-					'title': site_title
-				}
-			)
+            # Add the "fake" parent to the parents object, so that "DXP", "Commerce", etc.
+            # is visible in the breadcrumbs
+            doc_context["parents"].insert(0, {"link": site_parent, "title": site_title})
 
-		return doc_context
+        return doc_context
+
 
 def setup(app):
-	app.add_builder(WithRootSiteHTMLBuilder, True)
+    app.add_builder(WithRootSiteHTMLBuilder, True)
 
-	app.add_config_value('recommonmark_config', {
-		'enable_auto_toc_tree': False,
-		'enable_math': False,
-		'enable_inline_math': False
-	}, True)
+    app.add_config_value(
+        "recommonmark_config",
+        {
+            "enable_auto_toc_tree": False,
+            "enable_math": False,
+            "enable_inline_math": False,
+        },
+        True,
+    )
 
-	app.add_transform(AutoStructify)
+    app.add_transform(AutoStructify)
--- site/homepage/conf.py
+++ site/homepage/conf.py
@@ -3,14 +3,19 @@
 #
 
 author = "Liferay"
-copybutton_image_path="img/paste.svg"
+copybutton_image_path = "img/paste.svg"
 copyright = "2020, Liferay"
-exclude_patterns = ['node_modules']
-extensions = ["notfound.extension", "recommonmark", "sphinx_copybutton", "sphinx_markdown_tables"]
+exclude_patterns = ["node_modules"]
+extensions = [
+    "notfound.extension",
+    "recommonmark",
+    "sphinx_copybutton",
+    "sphinx_markdown_tables",
+]
 html_additional_pages = {"index": "index.html"}
 html_css_files = ["main.min.css"]
 html_favicon = "_static/img/favicon.ico"
-#html_js_files = ["main.js"]
+# html_js_files = ["main.js"]
 html_logo = "_static/img/liferay-waffle.svg"
 html_short_title = "Documentation"
 html_show_copyright = False
