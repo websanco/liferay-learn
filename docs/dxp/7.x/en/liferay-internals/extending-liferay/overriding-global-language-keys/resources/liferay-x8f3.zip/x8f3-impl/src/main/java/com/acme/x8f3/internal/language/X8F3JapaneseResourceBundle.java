e9dc5daf9a1792bee9f944cafd91d8a5de903317
mtokudome <makoto.tokudome@liferay.com>
2021-02-17 15:33:56 -0800

    LRDOCS-7658 added Portuguese and Japanese examples

---
+++ docs/dxp/7.x/en/liferay-internals/extending-liferay/overriding-global-language-keys/resources/liferay-x8f3.zip/x8f3-impl/src/main/java/com/acme/x8f3/internal/language/X8F3JapaneseResourceBundle.java
@@ -0,0 +1,26 @@
+package com.acme.x8f3.internal.language;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(property = "language.id=ja", service = ResourceBundle.class)
+public class X8F3JapaneseResourceBundle extends ResourceBundle {
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return _resourceBundle.getKeys();
+	}
+
+	@Override
+	protected Object handleGetObject(String key) {
+		return _resourceBundle.getObject(key);
+	}
+
+	private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
+		"content.Language_ja", UTF8Control.INSTANCE);
+
+}
---
+++ docs/dxp/7.x/en/liferay-internals/extending-liferay/overriding-global-language-keys/resources/liferay-x8f3.zip/x8f3-impl/src/main/java/com/acme/x8f3/internal/language/X8F3PortugueseResourceBundle.java
@@ -0,0 +1,26 @@
+package com.acme.x8f3.internal.language;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(property = "language.id=pt_BR", service = ResourceBundle.class)
+public class X8F3PortugueseResourceBundle extends ResourceBundle {
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return _resourceBundle.getKeys();
+	}
+
+	@Override
+	protected Object handleGetObject(String key) {
+		return _resourceBundle.getObject(key);
+	}
+
+	private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
+		"content.Language_pt_BR", UTF8Control.INSTANCE);
+
+}
