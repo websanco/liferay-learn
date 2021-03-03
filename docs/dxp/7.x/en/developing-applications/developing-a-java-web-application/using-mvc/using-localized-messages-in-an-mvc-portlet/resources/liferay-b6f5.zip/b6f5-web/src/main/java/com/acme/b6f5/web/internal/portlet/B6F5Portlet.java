c6632c86d33f5c33aede4076453a8cb58a8c16bb
mtokudome <makoto.tokudome@liferay.com>
2021-03-03 12:37:10 -0800

    LRDOCS-7774 removed init-param.template line

--- docs/dxp/7.x/en/developing-applications/developing-a-java-web-application/using-mvc/using-localized-messages-in-an-mvc-portlet/resources/liferay-b6f5.zip/b6f5-web/src/main/java/com/acme/b6f5/web/internal/portlet/B6F5Portlet.java
+++ docs/dxp/7.x/en/developing-applications/developing-a-java-web-application/using-mvc/using-localized-messages-in-an-mvc-portlet/resources/liferay-b6f5.zip/b6f5-web/src/main/java/com/acme/b6f5/web/internal/portlet/B6F5Portlet.java
@@ -10,7 +10,6 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"javax.portlet.display-name=B6F5 Portlet",
-		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.supported-locale=en_US,ja,pt_BR",
 		"javax.portlet.resource-bundle=content.Language"
