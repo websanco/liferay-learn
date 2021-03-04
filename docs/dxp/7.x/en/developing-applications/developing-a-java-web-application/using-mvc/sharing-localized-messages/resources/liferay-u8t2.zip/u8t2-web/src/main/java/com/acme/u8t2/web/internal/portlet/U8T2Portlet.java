ed201a5ec7d4a7bfde76eb628ea7e4bf56d64e43
jhinkey <james.hinkey@liferay.com>
2021-03-04 17:54:35 -0500

    LRDOCS-7770 sort Component properties

--- docs/dxp/7.x/en/developing-applications/developing-a-java-web-application/using-mvc/sharing-localized-messages/resources/liferay-u8t2.zip/u8t2-web/src/main/java/com/acme/u8t2/web/internal/portlet/U8T2Portlet.java
+++ docs/dxp/7.x/en/developing-applications/developing-a-java-web-application/using-mvc/sharing-localized-messages/resources/liferay-u8t2.zip/u8t2-web/src/main/java/com/acme/u8t2/web/internal/portlet/U8T2Portlet.java
@@ -11,8 +11,8 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.display-category=category.sample",
 		"javax.portlet.display-name=U8T2 Portlet",
 		"javax.portlet.init-param.view-template=/view.jsp",
-		"javax.portlet.supported-locale=en_US,ja,pt_BR",
-		"javax.portlet.resource-bundle=content.Language"
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.supported-locale=en_US,ja,pt_BR"
 	},
 	service = Portlet.class
 )
