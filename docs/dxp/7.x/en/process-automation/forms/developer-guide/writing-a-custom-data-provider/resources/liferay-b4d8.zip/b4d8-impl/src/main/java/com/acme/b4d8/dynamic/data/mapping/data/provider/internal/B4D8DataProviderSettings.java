5c32e1f9b509758bf8436b598c0b97423c961244
Russell Bohl <russell.bohl@liferay.com>
2021-03-11 17:49:07 -0500

    LRDOCS-7884 simplify the settings

--- docs/dxp/7.x/en/process-automation/forms/developer-guide/writing-a-custom-data-provider/resources/liferay-b4d8.zip/b4d8-impl/src/main/java/com/acme/b4d8/dynamic/data/mapping/data/provider/internal/B4D8DataProviderSettings.java
+++ docs/dxp/7.x/en/process-automation/forms/developer-guide/writing-a-custom-data-provider/resources/liferay-b4d8.zip/b4d8-impl/src/main/java/com/acme/b4d8/dynamic/data/mapping/data/provider/internal/B4D8DataProviderSettings.java
@@ -6,30 +6,9 @@ import com.liferay.dynamic.data.mapping.annotations.DDMFormLayout;
 import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutColumn;
 import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutPage;
 import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutRow;
-import com.liferay.dynamic.data.mapping.annotations.DDMFormRule;
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderParameterSettings;
 
-@DDMForm(
-	rules = {
-		@DDMFormRule(
-			actions = {
-				"setVisible('filterParameterName', true)",
-				"setRequired('filterParameterName', true)"
-			},
-			condition = "equals(getValue('filterable'), true)"
-		),
-		@DDMFormRule(
-			actions = {
-				"setVisible('pagination', false)",
-				"setVisible('paginationStartParameterName', false)",
-				"setVisible('paginationEndParameterName', false)",
-				"setRequired('paginationStartParameterName', equals(getValue('pagination'), true))",
-				"setRequired('paginationEndParameterName', equals(getValue('pagination'), true))"
-			},
-			condition = "TRUE"
-		)
-	}
-)
+@DDMForm
 @DDMFormLayout(
 	{
 		@DDMFormLayoutPage(
@@ -39,11 +18,7 @@ import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderParameterSe
 						@DDMFormLayoutColumn(
 							size = 12,
 							value = {
-								"url", "username", "password", "filterable",
-								"filterParameterName", "cacheable",
-								"pagination", "paginationStartParameterName",
-								"paginationEndParameterName", "timeout",
-								"inputParameters", "outputParameters"
+								"b4d8Boolean"
 							}
 						)
 					}
@@ -56,86 +31,8 @@ public interface B4D8DataProviderSettings
 	extends DDMDataProviderParameterSettings {
 
 	@DDMFormField(
-		label = "%cache-data-on-the-first-request",
+		label = "%b4d8-boolean",
 		properties = "showAsSwitcher=true"
 	)
-	public boolean cacheable();
-
-	@DDMFormField(
-		label = "%support-filtering-by-keyword",
-		properties = "showAsSwitcher=true"
-	)
-	public boolean filterable();
-
-	@DDMFormField(
-		label = "%filter-parameter-name",
-		properties = {
-			"placeholder=%enter-a-name-that-matches-one-of-the-rest-providers-parameters",
-			"tooltip=%the-parameter-whose-value-will-be-used-as-a-filter-by-the-rest-provider"
-		}
-	)
-	public String filterParameterName();
-
-	@DDMFormField(
-		label = "%support-pagination", predefinedValue = "false",
-		properties = "showAsSwitcher=true"
-	)
-	public boolean pagination();
-
-	@DDMFormField(
-		label = "%end-parameter-name", predefinedValue = "end",
-		properties = {
-			"placeholder=%enter-a-name-that-matches-one-of-the-rest-providers-parameters",
-			"tooltip=%the-parameter-whose-value-will-be-used-as-an-end-by-the-rest-provider"
-		}
-	)
-	public String paginationEndParameterName();
-
-	@DDMFormField(
-		label = "%start-parameter-name", predefinedValue = "start",
-		properties = {
-			"placeholder=%enter-a-name-that-matches-one-of-the-rest-providers-parameters",
-			"tooltip=%the-parameter-whose-value-will-be-used-as-a-start-by-the-rest-provider"
-		}
-	)
-	public String paginationStartParameterName();
-
-	@DDMFormField(
-		label = "%password",
-		properties = {
-			"placeholder=%enter-a-password",
-			"tooltip=%provide-the-password-for-authenticating-to-the-rest-provider"
-		},
-		type = "password"
-	)
-	public String password();
-
-	@DDMFormField(
-		dataType = "integer", label = "%timeout", predefinedValue = "1000",
-		properties = {
-			"placeholder=%enter-time-in-milliseconds",
-			"tooltip=%data-provider-timeout-message"
-		},
-		required = true, type = "numeric",
-		validationErrorMessage = "%please-enter-an-integer-between-1000-and-30000-milliseconds",
-		validationExpression = "(timeout >= 1000) && (timeout <= 30000)"
-	)
-	public String timeout();
-
-	@DDMFormField(
-		label = "%url", properties = "placeholder=%enter-the-rest-service-url",
-		required = true, validationErrorMessage = "%please-enter-a-valid-url",
-		validationExpression = "isURL(url)"
-	)
-	public String url();
-
-	@DDMFormField(
-		label = "%user-name",
-		properties = {
-			"placeholder=%enter-a-user-name",
-			"tooltip=%provide-the-user-name-for-authenticating-to-the-rest-provider"
-		}
-	)
-	public String username();
-
+	public boolean b4d8Boolean();
 }
--- docs/dxp/7.x/en/process-automation/forms/developer-guide/writing-a-custom-data-provider/resources/liferay-b4d8.zip/b4d8-impl/src/main/java/com/acme/b4d8/dynamic/data/mapping/data/provider/internal/B4D8DataProviderSettingsProvider.java
+++ docs/dxp/7.x/en/process-automation/forms/developer-guide/writing-a-custom-data-provider/resources/liferay-b4d8.zip/b4d8-impl/src/main/java/com/acme/b4d8/dynamic/data/mapping/data/provider/internal/B4D8DataProviderSettingsProvider.java
@@ -5,7 +5,7 @@ import org.osgi.service.component.annotations.Component;
 import com.liferay.dynamic.data.mapping.data.provider.settings.DDMDataProviderSettingsProvider;
 
 @Component(
-	immediate = true, property = "ddm.data.provider.type=rest",
+	immediate = true, property = "ddm.data.provider.type=b4d8",
 	service = DDMDataProviderSettingsProvider.class
 )
 public class B4D8DataProviderSettingsProvider
