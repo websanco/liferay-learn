2860ee77681112d6cf5b08b8b7eb269d2b0245df
jhinkey <james.hinkey@liferay.com>
2021-03-22 11:39:11 -0400

    LRDOCS-9156 Only copy the java or js templates if the project has a build.gradle or package.json respectively

--- docs/update_examples.sh
+++ docs/update_examples.sh
@@ -14,38 +14,38 @@ function copy_template {
 
 	for zip_dir_name in `find . -name ${zip_dir_name_pattern} -type d`
 	do
-		local maven_xml_file_name="$(echo $(find ${zip_dir_name} -name maven.xml -print) | head -n1)"
+		local gradle_build_file_name="$(echo $(find ${zip_dir_name} -name build.gradle -print) | head -n1)"
 
-		if [ -z "${maven_xml_file_name}" ]
+		if [ -n "${gradle_build_file_name}" ]
 		then
-			local gradle_build_file_name="$(echo $(find ${zip_dir_name} -name build.gradle -print) | head -n1)"
+			cp -fr _template/java/* ${zip_dir_name}
 
-			if [ -n "${gradle_build_file_name}" ]
+			if [ -n "$(grep release.dxp.api $(echo ${gradle_build_file_name}))" ]
 			then
-				cp -fr _template/java/* ${zip_dir_name}
-
-				if [ -n "$(grep release.dxp.api $(echo ${gradle_build_file_name}))" ]
-				then
-					echo -ne "liferay.workspace.product=${LIFERAY_LEARN_DXP_WORKSPACE_TOKEN}" > ${zip_dir_name}/gradle.properties
-				else
-					echo -ne "liferay.workspace.product=${LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN}" > ${zip_dir_name}/gradle.properties
-				fi
-
-				pushd ${zip_dir_name}
-
-				./gradlew classes formatSource
-
-				if [ "${1}" == "prod" ]
-				then
-					git clean -d -e "gradle.properties" -fx .
-
-					cp -fr $(git rev-parse --show-toplevel)/docs/_template/java/* .
-				fi
-
-				popd
+				echo -ne "liferay.workspace.product=${LIFERAY_LEARN_DXP_WORKSPACE_TOKEN}" > ${zip_dir_name}/gradle.properties
 			else
-				cp -fr _template/js/* ${zip_dir_name}
+				echo -ne "liferay.workspace.product=${LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN}" > ${zip_dir_name}/gradle.properties
 			fi
+
+			pushd ${zip_dir_name}
+
+			./gradlew classes formatSource
+
+			if [ "${1}" == "prod" ]
+			then
+				git clean -d -e "gradle.properties" -fx .
+
+				cp -fr $(git rev-parse --show-toplevel)/docs/_template/java/* .
+			fi
+
+			popd
+		fi
+
+		local package_json_file_name="$(echo $(find ${zip_dir_name} -name package.json -print) | head -n1)"
+
+		if [ -n "${package_json_file_name}" ]
+		then
+			cp -fr _template/js/* ${zip_dir_name}
 		fi
 	done
 }
