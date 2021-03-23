1bc65cdab33bf242dd928923f180c375962cb330
jhinkey <james.hinkey@liferay.com>
2021-03-23 14:57:09 -0400

    LRDOCS-9156 curl commands updates: use double quotes, add file document_add.sh, remove -X for GET commands, and use ${param names} per UI.

--- docs/dxp/7.x/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/document_delete.sh
+++ docs/dxp/7.x/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/document_delete.sh
@@ -1,1 +1,1 @@
-curl -X DELETE "http://localhost:8080/o/headless-delivery/v1.0/documents/${1}" -u 'test@liferay.com:test'
+curl -X DELETE "http://localhost:8080/o/headless-delivery/v1.0/documents/${documentId}" -u "test@liferay.com:test"
