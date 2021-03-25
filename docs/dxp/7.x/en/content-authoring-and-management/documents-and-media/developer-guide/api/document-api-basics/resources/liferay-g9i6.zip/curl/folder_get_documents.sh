514b829a5f5bf112c6dd59ee12e6b8950ce03189
jhinkey <james.hinkey@liferay.com>
2021-03-25 12:35:14 -0400

    LRDOCS-9166 Pair down the number of services

--- docs/dxp/7.x/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/folder_get_documents.sh
+++
@@ -1,1 +0,0 @@
-curl "http://localhost:8080/o/headless-delivery/v1.0/document-folders/${1}/documents" -u "test@liferay.com:test"
--- docs/dxp/7.x/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/folder_get_subfolders.sh
+++
@@ -1,1 +0,0 @@
-curl "http://localhost:8080/o/headless-delivery/v1.0/document-folders/${1}/document-folders" -u "test@liferay.com:test"
