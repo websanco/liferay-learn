0ac9f53a14c396bf2ed1b65992d9c8c909d5b0d3
Brian Chan <brian.chan@liferay.com>
2021-03-26 09:00:14 -0300

    LRDOCS-8379 Inline

--- docs/dxp/7.x/en/headless-delivery/producing-apis-with-rest-builder/implementing-a-new-api-with-rest-builder/resources/liferay-r3b2.zip/headless-r3b2-impl/src/main/java/com/acme/headless/r3b2/internal/resource/v1_0/BarResourceImpl.java
+++ docs/dxp/7.x/en/headless-delivery/producing-apis-with-rest-builder/implementing-a-new-api-with-rest-builder/resources/liferay-r3b2.zip/headless-r3b2-impl/src/main/java/com/acme/headless/r3b2/internal/resource/v1_0/BarResourceImpl.java
@@ -6,6 +6,7 @@ import com.acme.headless.r3b2.resource.v1_0.BarResource;
 import com.liferay.portal.vulcan.pagination.Page;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
@@ -22,17 +23,13 @@ public class BarResourceImpl extends BaseBarResourceImpl {
 
 	@Override
 	public Page<Bar> getFooBars(Integer fooId) {
-		if (_bars == null) {
-			_initBarList();
-		}
-
 		List<Bar> bars = _getBarsByFoo(fooId);
 
 		return Page.of(bars);
 	}
 
 	private List<Bar> _getBarsByFoo(int fooId) {
-		List<Bar> bars = new ArrayList();
+		List<Bar> bars = new ArrayList<>();
 
 		for (Bar check : _bars) {
 			if (check.getFooId() == fooId) {
@@ -43,63 +40,58 @@ public class BarResourceImpl extends BaseBarResourceImpl {
 		return bars;
 	}
 
-	private void _initBarList() {
-		Bar faith = new Bar();
-
-		faith.setId(1);
-		faith.setFooId(1);
-		faith.setName("Faith");
-		faith.setDescription(
-			"Faith is the substance of things hoped for, the evidence of " +
-				"things not seen.");
-
-		Bar hope = new Bar();
-
-		hope.setId(2);
-		hope.setFooId(2);
-		hope.setName("Hope");
-		hope.setDescription("Lay hold of the hope set before us.");
-
-		Bar love = new Bar();
-
-		love.setId(3);
-		love.setFooId(3);
-		love.setName("Love");
-		love.setDescription(
-			"Now abide faith, hope, love, these three; but the greatest of " +
-				"these is love.");
-
-		Bar joy = new Bar();
-
-		joy.setId(4);
-		joy.setFooId(1);
-		joy.setName("Joy");
-		joy.setDescription("Enter into the joy of your lord.");
-
-		Bar peace = new Bar();
-
-		peace.setId(5);
-		peace.setFooId(2);
-		peace.setName("Peace");
-		peace.setDescription("Peace to you!");
-
-		Bar patience = new Bar();
-
-		patience.setId(6);
-		patience.setFooId(3);
-		patience.setName("Patience");
-		patience.setDescription("Faith produces patience.");
-
-		_bars = new ArrayList();
-
-		_bars.add(faith);
-		_bars.add(hope);
-		_bars.add(love);
-		_bars.add(joy);
-		_bars.add(peace);
-		_bars.add(patience);
-	}
-
-	private List<Bar> _bars;
+	private List<Bar> _bars = Arrays.asList(
+		new Bar() {
+			{
+				description =
+					"Faith is the substance of things hoped for, the " +
+						"evidence of things not seen.";
+				fooId = 1;
+				id = 1;
+				name = "Faith";
+			}
+		},
+		new Bar() {
+			{
+				description = "Lay hold of the hope set before us.";
+				fooId = 2;
+				id = 2;
+				name = "Hope";
+			}
+		},
+		new Bar() {
+			{
+				description =
+					"Now abide faith, hope, love, these three; but the " +
+						"greatest of these is love.";
+				fooId = 3;
+				id = 3;
+				name = "Love";
+			}
+		},
+		new Bar() {
+			{
+				description = "Enter into the joy of your lord.";
+				fooId = 1;
+				id = 4;
+				name = "Joy";
+			}
+		},
+		new Bar() {
+			{
+				description = "Peace to you!";
+				fooId = 2;
+				id = 5;
+				name = "Peace";
+			}
+		},
+		new Bar() {
+			{
+				description = "Faith produces patience.";
+				fooId = 3;
+				id = 6;
+				name = "Patience";
+			}
+		});
 
 }
