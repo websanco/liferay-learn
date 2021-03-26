3f4b4aafdd6a730a6a18cd19d07bc2ced5a12706
jhinkey <james.hinkey@liferay.com>
2021-03-26 12:12:24 -0400

    LRDOCS-9166 Apply class file name convention to .sh files.

--- docs/dxp/7.x/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/document_PUT_to_document_folder.sh
+++
@@ -1,1 +0,0 @@
-curl -F "file=@document_PUT_to_document_folder.sh" -H "Content-Type: multipart/form-data" -X POST "http://localhost:8080/o/headless-delivery/v1.0/document-folders/${1}/documents" -u "test@liferay.com:test"
--- docs/dxp/7.x/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/document_PUT_to_site.sh
+++
@@ -1,1 +0,0 @@
-curl -F "file=@document_PUT_to_site.sh" -H "Content-Type: multipart/form-data" -X POST "http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/documents" -u "test@liferay.com:test"
