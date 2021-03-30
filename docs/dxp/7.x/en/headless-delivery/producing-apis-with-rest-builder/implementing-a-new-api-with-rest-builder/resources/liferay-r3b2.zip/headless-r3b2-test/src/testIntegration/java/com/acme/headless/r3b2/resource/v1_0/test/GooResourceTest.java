07b3629502570912c8687f376bcf006d0775855a
Brian Chan <brian.chan@liferay.com>
2021-03-30 08:25:30 -0300

    Add client, test, etc.

---
+++ docs/dxp/7.x/en/headless-delivery/producing-apis-with-rest-builder/implementing-a-new-api-with-rest-builder/resources/liferay-r3b2.zip/headless-r3b2-test/src/testIntegration/java/com/acme/headless/r3b2/resource/v1_0/test/GooResourceTest.java
@@ -0,0 +1,14 @@
+package com.acme.headless.r3b2.resource.v1_0.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jonah the son of Amittai
+ */
+@Ignore
+@RunWith(Arquillian.class)
+public class GooResourceTest extends BaseGooResourceTestCase {
+}
