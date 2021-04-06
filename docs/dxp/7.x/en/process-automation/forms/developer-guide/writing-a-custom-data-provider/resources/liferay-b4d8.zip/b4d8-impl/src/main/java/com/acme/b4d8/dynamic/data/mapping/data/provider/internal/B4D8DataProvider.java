fd94f727540f352a5f4db496d3a6d36b815414d3
Russell Bohl <russell.bohl@liferay.com>
2021-04-05 21:34:35 -0400

    LRDOCS-7884 editing

--- docs/dxp/7.x/en/process-automation/forms/developer-guide/writing-a-custom-data-provider/resources/liferay-b4d8.zip/b4d8-impl/src/main/java/com/acme/b4d8/dynamic/data/mapping/data/provider/internal/B4D8DataProvider.java
+++ docs/dxp/7.x/en/process-automation/forms/developer-guide/writing-a-custom-data-provider/resources/liferay-b4d8.zip/b4d8-impl/src/main/java/com/acme/b4d8/dynamic/data/mapping/data/provider/internal/B4D8DataProvider.java
@@ -7,31 +7,37 @@ import com.liferay.dynamic.data.mapping.data.provider.DDMDataProvider;
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderException;
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderRequest;
 import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderResponse;
-import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderResponseStatus;
 import com.liferay.dynamic.data.mapping.data.provider.settings.DDMDataProviderSettingsProvider;
+import com.liferay.dynamic.data.mapping.service.DDMDataProviderInstanceService;
 
-@Component(
-	immediate = true, property = "ddm.data.provider.type=b4d8",
-	service = DDMDataProvider.class
-)
+@Component(immediate = true, property = "ddm.data.provider.type=b4d8", service = DDMDataProvider.class)
 public class B4D8DataProvider implements DDMDataProvider {
 
 	@Override
 	public DDMDataProviderResponse getData(DDMDataProviderRequest ddmDataProviderRequest)
 			throws DDMDataProviderException {
 
+//		DDMDataProviderResponse restResponse = ddmRESTDataProvider.getData(ddmDataProviderRequest);
+		
 		DDMDataProviderResponse.Builder builder = DDMDataProviderResponse.Builder.newBuilder();
-		
-		System.out.println("hi from the B4D8 data provider, beep boop");
-		
-		return builder.withOutput("hello", builder).withStatus(DDMDataProviderResponseStatus.OK).build();
+
+		return builder.withOutput("id","value").build();
 	}
 
 	@Override
 	public Class<?> getSettings() {
-		return ddmDataProviderSettingsProvider.getSettings();
+		return ddmRESTDataProviderSettingsProvider.getSettings();
 	}
-	
+
+//	@Reference (target = "(ddm.data.provider.type=rest)")
+//	protected DDMDataProvider ddmRESTDataProvider;
+
+	@Reference
+	protected DDMDataProviderInstanceService ddmDataProviderInstanceService;
+
 	@Reference(target = "(ddm.data.provider.type=b4d8)")
 	protected DDMDataProviderSettingsProvider ddmDataProviderSettingsProvider;
+
+//	@Reference(target = "(ddm.data.provider.type=rest)")
+//	protected DDMDataProviderSettingsProvider ddmRESTDataProviderSettingsProvider;
 }
