37128dd88b77e97a58e2dbb380c866db3383ae5b
jhinkey <james.hinkey@liferay.com>
2021-04-21 14:04:29 -0400

    LRDOCS-9319 Start continued lines with a tab

--- docs/dxp/latest/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/Document_GET_ById_ContentValue.sh
+++ docs/dxp/latest/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/Document_GET_ById_ContentValue.sh
@@ -1,7 +1,7 @@
 curl \
- "http://localhost:8080/o/headless-delivery/v1.0/documents/${1}?nestedFields=contentValue&fields=contentValue" \
- -u "test@liferay.com:test" \
- | sed -n '2 p' \
- | awk -F':' '{print $2}' \
- | tr -d ' "' \
- | base64 -d
+	"http://localhost:8080/o/headless-delivery/v1.0/documents/${1}?nestedFields=contentValue&fields=contentValue" \
+	-u "test@liferay.com:test" \
+	| sed -n '2 p' \
+	| awk -F':' '{print $2}' \
+	| tr -d ' "' \
+	| base64 -d
