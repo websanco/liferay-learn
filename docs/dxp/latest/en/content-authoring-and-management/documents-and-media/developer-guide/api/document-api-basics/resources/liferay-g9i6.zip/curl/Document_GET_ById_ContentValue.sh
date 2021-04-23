27af519deb778380afdb39cce86f064e0c4499f1
jhinkey <james.hinkey@liferay.com>
2021-04-23 10:59:51 -0400

    LRDOCS-9319 awk processing instruction must be in single quotes

--- docs/dxp/latest/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/Document_GET_ById_ContentValue.sh
+++ docs/dxp/latest/en/content-authoring-and-management/documents-and-media/developer-guide/api/document-api-basics/resources/liferay-g9i6.zip/curl/Document_GET_ById_ContentValue.sh
@@ -2,6 +2,6 @@ curl \
 	"http://localhost:8080/o/headless-delivery/v1.0/documents/${1}?nestedFields=contentValue&fields=contentValue" \
 	-u "test@liferay.com:test" \
 	| sed -n "2 p" \
-	| awk -F ":" "{print $2}" \
+	| awk -F ":" '{print $2}' \
 	| tr -d ' "' \
 	| base64 -d
