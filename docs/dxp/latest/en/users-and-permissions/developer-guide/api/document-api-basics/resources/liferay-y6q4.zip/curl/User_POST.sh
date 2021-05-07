05091e247b90d4ea88f311d32b4b638da040c024
mtokudome <makoto.tokudome@liferay.com>
2021-05-07 11:08:59 -0700

    LRDOCS-9301 more sourceFormatter

--- docs/dxp/latest/en/users-and-permissions/developer-guide/api/document-api-basics/resources/liferay-y6q4.zip/curl/User_POST.sh
+++ docs/dxp/latest/en/users-and-permissions/developer-guide/api/document-api-basics/resources/liferay-y6q4.zip/curl/User_POST.sh
@@ -1,5 +1,5 @@
 curl \
-  -H "Content-Type: application/json" \
-  -X POST "http://localhost:8080/o/headless-admin-user/v1.0/user-accounts" \
-  -d "{\"alternateName\":\"Foo\",\"emailAddress\":\"foo@liferay.com\",\"familyName\":\"Liferay\",\"givenName\":\"Foo\"}" \
-  -u "test@liferay.com:test"
+-H "Content-Type: application/json" \
+-X POST "http://localhost:8080/o/headless-admin-user/v1.0/user-accounts" \
+-d "{\"alternateName\":\"Foo\",\"emailAddress\":\"foo@liferay.com\",\"familyName\":\"Liferay\",\"givenName\":\"Foo\"}" \
+-u "test@liferay.com:test"
