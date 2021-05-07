ed3ae04e419136cd949bb14841ecd530ea0448d9
mtokudome <makoto.tokudome@liferay.com>
2021-05-07 10:48:26 -0700

    LRDOCS-9301 userId change

--- docs/dxp/latest/en/users-and-permissions/developer-guide/api/document-api-basics/resources/liferay-y6q4.zip/java/User_GET_ById.java
+++ docs/dxp/latest/en/users-and-permissions/developer-guide/api/document-api-basics/resources/liferay-y6q4.zip/java/User_GET_ById.java
@@ -4,7 +4,7 @@ import com.liferay.headless.admin.user.client.resource.v1_0.UserAccountResource;
 public class User_GET_ById {
 
   /**
-	 * java -classpath .:* -DuserId=38138 User_GET_ById
+	 * java -classpath .:* -DuserId=1234 User_GET_ById
 	 */
    public static void main(String[] args) throws Exception {
      UserAccountResource.Builder builder = UserAccountResource.builder();
