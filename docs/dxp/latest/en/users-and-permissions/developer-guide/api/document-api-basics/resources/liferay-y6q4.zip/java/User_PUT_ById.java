3e8c24be5f91fcc1b346df0f156997d8e79e1c96
mtokudome <makoto.tokudome@liferay.com>
2021-05-07 15:51:47 -0700

    LRDOCS-9301 added User_PUT

---
+++ docs/dxp/latest/en/users-and-permissions/developer-guide/api/document-api-basics/resources/liferay-y6q4.zip/java/User_PUT_ById.java
@@ -0,0 +1,30 @@
+import com.liferay.headless.admin.user.client.dto.v1_0.UserAccount;
+import com.liferay.headless.admin.user.client.resource.v1_0.UserAccountResource;
+
+public class User_PUT_ById {
+
+	/**
+	 * java -classpath .:* -DuserId=1234 User_PUT_ById
+	 */
+	public static void main(String[] args) throws Exception {
+		UserAccountResource.Builder builder = UserAccountResource.builder();
+
+		UserAccountResource userAccountResource = builder.authentication(
+			"test@liferay.com", "test"
+		).build();
+
+		UserAccount userAccount = userAccountResource.putUserAccount(
+			Long.valueOf(System.getProperty("userId")),
+			new UserAccount() {
+				{
+					alternateName = "Bar";
+					emailAddress = "bar@liferay.com";
+					familyName = "Liferay";
+					givenName = "Bar";
+				}
+			});
+
+		System.out.println(userAccount);
+	}
+
+}
