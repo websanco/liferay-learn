63a37de494dfa2354d00b5649ac7726c9477cffa
JR Houn <jr.houn@liferay.com>
2021-05-14 11:29:30 -0700

    Update portal version to 7.4

--- _common.sh
+++ _common.sh
@@ -9,10 +9,10 @@ readonly LIFERAY_LEARN_DXP_DOCKER_IMAGE_TOKEN=\\[\$LIFERAY_LEARN_DXP_DOCKER_IMAG
 readonly LIFERAY_LEARN_DXP_DOCKER_IMAGE_VALUE=liferay\\/dxp\\:7.3.10-dxp-1
 readonly LIFERAY_LEARN_DXP_WORKSPACE_TOKEN=dxp-7.3-sp1
 readonly LIFERAY_LEARN_PORTAL_DOCKER_IMAGE_TOKEN=\\[\$LIFERAY_LEARN_PORTAL_DOCKER_IMAGE\$\\]
-readonly LIFERAY_LEARN_PORTAL_DOCKER_IMAGE_VALUE=liferay\\/portal\\:7.3.6-ga7
+readonly LIFERAY_LEARN_PORTAL_DOCKER_IMAGE_VALUE=liferay\\/portal\\:7.4.0-ga1
 readonly LIFERAY_LEARN_PORTAL_GIT_TAG_TOKEN=\\[\$LIFERAY_LEARN_PORTAL_GIT_TAG\$\\]
-readonly LIFERAY_LEARN_PORTAL_GIT_TAG_VALUE=7.3.6-ga7
-readonly LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN=portal-7.3-ga7
+readonly LIFERAY_LEARN_PORTAL_GIT_TAG_VALUE=7.4.0-ga1
+readonly LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN=portal-7.4-ga1
 
 function download_nexus_jar {
 	curl -L "https://repository-cdn.liferay.com/nexus/service/local/artifact/maven/redirect?a=${1}&g=com.liferay&r=liferay-public-releases&v=LATEST" -o $(ls -d liferay*.zip | head -n1)/java/${1}.jar
