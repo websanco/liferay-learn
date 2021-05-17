3b08366e54485a9f9ed68304e9516f21b2cecd2a
jhinkey <james.hinkey@liferay.com>
2021-05-17 09:22:01 -0400

    LRDOCS-9396 Refactor the code

--- docs/dxp/latest/en/content-authoring-and-management/documents-and-media/developer-guide/creating-custom-video-shortcut-providers/resources/liferay-g9b6.zip/g9b6-impl/src/main/java/com/acme/g9b6/document/library/internal/video/external/shortcut/provider/G9B6DLVideoExternalShortcutProvider.java
+++ docs/dxp/latest/en/content-authoring-and-management/documents-and-media/developer-guide/creating-custom-video-shortcut-providers/resources/liferay-g9b6.zip/g9b6-impl/src/main/java/com/acme/g9b6/document/library/internal/video/external/shortcut/provider/G9B6DLVideoExternalShortcutProvider.java
@@ -3,55 +3,49 @@ package com.acme.g9b6.document.library.internal.video.external.shortcut.provider
 import com.liferay.document.library.video.external.shortcut.DLVideoExternalShortcut;
 import com.liferay.document.library.video.external.shortcut.provider.DLVideoExternalShortcutProvider;
 import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.util.ContentTypes;
-import com.liferay.portal.kernel.util.Http;
-import com.liferay.portal.kernel.util.Validator;
-import java.net.HttpURLConnection;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
+
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 @Component(service = DLVideoExternalShortcutProvider.class)
 public class G9B6DLVideoExternalShortcutProvider
-    implements DLVideoExternalShortcutProvider {
+	implements DLVideoExternalShortcutProvider {
 
-    @Override
-    public DLVideoExternalShortcut getDLVideoExternalShortcut(String url) {
-        String dailymotionVideoId;
-        Pattern urlPattern = Pattern.compile(
-            "https?:\\/\\/(?:www\\.)?dai\\.ly\\/(\\S*)$");
-            Matcher matcher = urlPattern.matcher(url);
+	@Override
+	public DLVideoExternalShortcut getDLVideoExternalShortcut(String url) {
+		Matcher matcher = _urlPattern.matcher(url);
 
-        if (matcher.matches()) {
-            dailymotionVideoId = matcher.group(1);
-        } else {
-            dailymotionVideoId = null;
-        }
+		if (!matcher.matches()) {
+			return null;
+		}
 
-        if (Validator.isNull(dailymotionVideoId)) {
-            return null;
-        }
+		return new DLVideoExternalShortcut() {
 
-        return new DLVideoExternalShortcut() {
+			@Override
+			public String getURL() {
+				return url;
+			}
 
-            @Override
-            public String getURL() {
-                return url;
-            }
+			@Override
+			public String renderHTML(HttpServletRequest httpServletRequest) {
+				String iframeSrc =
+					"https://www.dailymotion.com/embed/video/" +
+						matcher.group(1) + "?rel=0";
 
-            @Override
-            public String renderHTML(HttpServletRequest httpServletRequest) {
-                String iframeSrc =
-                    "https://www.dailymotion.com/embed/video/" + dailymotionVideoId +
-                        "?rel=0";
+				return StringBundler.concat(
+					"<iframe allow=\"autoplay; encrypted-media\" ",
+					"allowfullscreen height=\"315\" frameborder=\"0\" ",
+					"src=\"", iframeSrc, "\" width=\"560\"></iframe>");
+			}
 
-                return StringBundler.concat(
-                    "<iframe allow=\"autoplay; encrypted-media\" ",
-                    "allowfullscreen height=\"315\" frameborder=\"0\" ",
-                    "src=\"", iframeSrc, "\" width=\"560\"></iframe>");
-            }
-        };
-    }
+		};
+	}
+
+	private static final Pattern _urlPattern = Pattern.compile(
+		"https?:\\/\\/(?:www\\.)?dai\\.ly\\/(\\S*)$");
+
 }
