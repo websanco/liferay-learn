979cfdd23704bf5204fcc8067fa51d514a0dbe12
mtokudome <makoto.tokudome@liferay.com>
2021-05-25 14:57:46 -0700

    LRDOCS-9470 switched optionLabels and optionValues

--- docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/providing-dynamic-dropdown-values/resources/liferay-z4h3.zip/z4h3-impl/src/main/java/com/acme/z4h3/internal/configuration/Z4H3Configuration.java
+++ docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/providing-dynamic-dropdown-values/resources/liferay-z4h3.zip/z4h3-impl/src/main/java/com/acme/z4h3/internal/configuration/Z4H3Configuration.java
@@ -11,16 +11,16 @@ public @interface Z4H3Configuration {
 	@Meta.AD(
 		description = "this-list-is-manually-populated",
 		name = "z4h3-asset-types",
-		optionLabels = {"option1", "option2", "option3"},
-		optionValues = {"Option 1", "Option 2", "Option 3"}, required = false
+		optionLabels = {"Option 1", "Option 2", "Option 3"},
+		optionValues = {"option1", "option2", "option3"}, required = false
 	)
 	String entryList1();
 
 	@Meta.AD(
 		description = "this-list-is-dynamically-populated",
 		name = "z4h3-asset-types",
-		optionLabels = {"option1", "option2", "option3"},
-		optionValues = {"Option 1", "Option 2", "Option 3"}, required = false
+		optionLabels = {"Option 1", "Option 2", "Option 3"},
+		optionValues = {"option1", "option2", "option3"}, required = false
 	)
 	String entryList2();
 
