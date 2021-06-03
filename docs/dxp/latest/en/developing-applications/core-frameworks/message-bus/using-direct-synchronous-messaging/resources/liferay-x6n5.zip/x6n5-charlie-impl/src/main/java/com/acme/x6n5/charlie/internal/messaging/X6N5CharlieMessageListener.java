a7d0efa06ccb76633a2d32dad6c981cfd59a1ba9
jhinkey <james.hinkey@liferay.com>
2021-06-03 14:47:37 -0400

    LRDOCS-9476 Show using setResponse. Remove response destination code.

--- docs/dxp/latest/en/developing-applications/core-frameworks/message-bus/using-direct-synchronous-messaging/resources/liferay-x6n5.zip/x6n5-charlie-impl/src/main/java/com/acme/x6n5/charlie/internal/messaging/X6N5CharlieMessageListener.java
+++ docs/dxp/latest/en/developing-applications/core-frameworks/message-bus/using-direct-synchronous-messaging/resources/liferay-x6n5.zip/x6n5-charlie-impl/src/main/java/com/acme/x6n5/charlie/internal/messaging/X6N5CharlieMessageListener.java
@@ -3,12 +3,9 @@ package com.acme.x6n5.charlie.internal.messaging;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.messaging.MessageListener;
-import com.liferay.portal.kernel.messaging.sender.SynchronousMessageSender;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 @Component(
 	property = "destination.name=acme/x6n5_able",
@@ -24,27 +21,10 @@ public class X6N5CharlieMessageListener implements MessageListener {
 			_log.info("Received message payload " + payload.toString());
 		}
 
-		// message.setResponse("X6N5CharlieMessageListener");
-
-		Message responseMessage = new Message();
-
-		responseMessage.setDestinationName(
-			message.getResponseDestinationName());
-		responseMessage.setPayload(
-			"X6N5CharlieMessageListener");
-		responseMessage.setResponseId(message.getResponseId());
-
-		_messageBus.sendMessage(
-			message.getResponseDestinationName(), responseMessage);
+		message.setResponse("X6N5CharlieMessageListener");
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		X6N5CharlieMessageListener.class);
-
-	@Reference(target = "(mode=DIRECT)")
-	private SynchronousMessageSender _directSynchronousMessageSender;
-
-	@Reference
-	private MessageBus _messageBus;
 
 }
--- docs/dxp/latest/en/developing-applications/core-frameworks/message-bus/using-direct-synchronous-messaging/resources/liferay-x6n5.zip/x6n5-dog-impl/src/main/java/com/acme/x6n5/dog/internal/messaging/X6N5DogMessageListener.java
+++ docs/dxp/latest/en/developing-applications/core-frameworks/message-bus/using-direct-synchronous-messaging/resources/liferay-x6n5.zip/x6n5-dog-impl/src/main/java/com/acme/x6n5/dog/internal/messaging/X6N5DogMessageListener.java
@@ -3,12 +3,9 @@ package com.acme.x6n5.dog.internal.messaging;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.messaging.MessageBus;
 import com.liferay.portal.kernel.messaging.MessageListener;
-import com.liferay.portal.kernel.messaging.sender.SynchronousMessageSender;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 @Component(
 	property = "destination.name=acme/x6n5_able",
@@ -23,15 +20,11 @@ public class X6N5DogMessageListener implements MessageListener {
 
 			_log.info("Received message payload " + payload.toString());
 		}
+
+		message.setResponse("X6N5DogMessageListener");
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		X6N5DogMessageListener.class);
-
-	@Reference(target = "(mode=DIRECT)")
-	private SynchronousMessageSender _directSynchronousMessageSender;
-
-	@Reference
-	private MessageBus _messageBus;
 
 }
