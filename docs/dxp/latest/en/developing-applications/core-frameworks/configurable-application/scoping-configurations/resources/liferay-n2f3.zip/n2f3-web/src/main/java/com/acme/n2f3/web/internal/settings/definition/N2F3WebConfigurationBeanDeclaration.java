1826c5c8dc7bac894e03036d369bbe9f80f2f639
mtokudome <makoto.tokudome@liferay.com>
2021-07-01 11:03:41 -0700

    LRDOCS-9474 added attribute

--- docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/scoping-configurations/resources/liferay-n2f3.zip/n2f3-web/src/main/java/com/acme/n2f3/web/internal/settings/definition/N2F3WebConfigurationBeanDeclaration.java
+++ docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/scoping-configurations/resources/liferay-n2f3.zip/n2f3-web/src/main/java/com/acme/n2f3/web/internal/settings/definition/N2F3WebConfigurationBeanDeclaration.java
@@ -6,8 +6,8 @@ import com.liferay.portal.kernel.settings.definition.ConfigurationBeanDeclaratio
 
 import org.osgi.service.component.annotations.Component;
 
-@Component
-public class N2F3ConfigurationBeanDeclaration
+@Component(service = ConfigurationBeanDeclaration.class)
+public class N2F3WebConfigurationBeanDeclaration
 	implements ConfigurationBeanDeclaration {
 
 	@Override
