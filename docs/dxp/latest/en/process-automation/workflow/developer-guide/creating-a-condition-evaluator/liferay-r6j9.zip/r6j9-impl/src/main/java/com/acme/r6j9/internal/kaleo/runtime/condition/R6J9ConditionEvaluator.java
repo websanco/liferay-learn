0d9dc0597b677326414af78bd5332b3ac047be0d
Russell Bohl <russell.bohl@liferay.com>
2021-07-15 18:10:07 -0400

    LRDOCS-9523 initial implementation; if the workflow user has the administrator role, trigger the "approved" transition; else "review"

--- docs/dxp/latest/en/process-automation/workflow/developer-guide/creating-a-condition-evaluator/liferay-r6j9.zip/r6j9-impl/src/main/java/com/acme/r6j9/internal/kaleo/runtime/condition/R6J9ConditionEvaluator.java
+++ docs/dxp/latest/en/process-automation/workflow/developer-guide/creating-a-condition-evaluator/liferay-r6j9.zip/r6j9-impl/src/main/java/com/acme/r6j9/internal/kaleo/runtime/condition/R6J9ConditionEvaluator.java
@@ -1,11 +1,21 @@
 package com.acme.r6j9.internal.kaleo.runtime.condition;
 
-import org.osgi.service.component.annotations.Component;
-
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.workflow.kaleo.model.KaleoCondition;
 import com.liferay.portal.workflow.kaleo.runtime.ExecutionContext;
 import com.liferay.portal.workflow.kaleo.runtime.condition.ConditionEvaluator;
+
+import java.io.Serializable;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 @Component(
 	immediate = true, property = "scripting.language=java",
@@ -18,8 +28,27 @@ public class R6J9ConditionEvaluator implements ConditionEvaluator {
 			KaleoCondition kaleoCondition, ExecutionContext executionContext)
 		throws PortalException {
 
-		// logic here
-		return "approved";
+		// check if the user is a regular administrator, and approve
+		// automatically if so.
+
+		Map<String, Serializable> workflowContext =
+			executionContext.getWorkflowContext();
+		ServiceContext serviceContext = executionContext.getServiceContext();
+
+		long userId = GetterUtil.getLong(
+			workflowContext.get(WorkflowConstants.CONTEXT_USER_ID));
+
+		if (_userLocalService.hasRoleUser(
+				serviceContext.getCompanyId(), RoleConstants.ADMINISTRATOR,
+				userId, false)) {
+
+			return "approve";
+		}
+
+		return "review";
 	}
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
