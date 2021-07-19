4f65239af9596e954f82f7e778683c46811e6837
jhinkey <james.hinkey@liferay.com>
2021-07-19 17:23:59 -0400

    LRDOCS-9608 Sort

--- docs/dxp/latest/en/content-authoring-and-management/blogs/developer-guide/blog-api-basics/resources/liferay-r3g4.zip/curl/BlogPosting_POST_ToSite.sh
+++ docs/dxp/latest/en/content-authoring-and-management/blogs/developer-guide/blog-api-basics/resources/liferay-r3g4.zip/curl/BlogPosting_POST_ToSite.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/blog-postings" \
-	-d "{\"headline\": \"Able\", \"articleBody\": \"Foo\"}" \
+	-d "{\"articleBody\": \"Foo\", \"headline\": \"Able\"}" \
 	-u "test@liferay.com:test"
--- docs/dxp/latest/en/content-authoring-and-management/blogs/developer-guide/blog-api-basics/resources/liferay-r3g4.zip/curl/BlogPosting_PUT_ById.sh
+++ docs/dxp/latest/en/content-authoring-and-management/blogs/developer-guide/blog-api-basics/resources/liferay-r3g4.zip/curl/BlogPosting_PUT_ById.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X PUT \
 	"http://localhost:8080/o/headless-delivery/v1.0/blog-postings/${1}" \
-	-d "{\"headline\": \"Able\", \"articleBody\": \"Goo\"}" \
+	-d "{\"articleBody\": \"Goo\", \"headline\": \"Able\"}" \
 	-u "test@liferay.com:test"
