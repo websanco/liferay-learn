a558d8f123b9ee04b4ee938949a8bec23ba24247
Jorge Campo <62282406+jorge-campo@users.noreply.github.com>
2021-07-23 12:25:31 +0200

    LRDOCS-9606 StructuredContents GET, POST, and DELETE scripts

---
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContents_DELETE_FromSite.sh
@@ -0,0 +1,3 @@
+curl -X 'DELETE' \
+  "http://localhost:8080/o/headless-delivery/v1.0/structured-contents/${1}" \
+  -u "test@liferay.com:test"
---
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContents_GET_FromSite.sh
@@ -0,0 +1,3 @@
+curl \
+	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/structured-contents" \
+	-u "test@liferay.com:test"
