84d136419ec4e2a81557ff4c356fcc6648ce039d
Russell Bohl <russell.bohl@liferay.com>
2021-07-28 14:19:10 -0400

    LRDOCS-9253 simplifying the workflow loading code

--- docs/dxp/latest/en/process-automation/workflow/developer-guide/creating-a-condition-evaluator/resources/liferay-r6j9.zip/r6j9-impl/src/main/java/com/acme/r6j9/internal/kaleo/runtime/condition/R6J9ConditionEvaluator.java
+++ docs/dxp/latest/en/process-automation/workflow/developer-guide/creating-a-condition-evaluator/resources/liferay-r6j9.zip/r6j9-impl/src/main/java/com/acme/r6j9/internal/kaleo/runtime/condition/R6J9ConditionEvaluator.java
@@ -2,18 +2,15 @@ package com.acme.r6j9.internal.kaleo.runtime.condition;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.role.RoleConstants;
-import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.workflow.WorkflowDefinitionManager;
 import com.liferay.portal.workflow.kaleo.model.KaleoCondition;
@@ -24,7 +21,6 @@ import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
 import java.io.InputStream;
 import java.io.Serializable;
 
-import java.util.List;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Activate;
@@ -60,62 +56,31 @@ public class R6J9ConditionEvaluator implements ConditionEvaluator {
 
 	@Activate
 	protected void activate() throws Exception {
-		List<Company> companies = _companyLocalService.getCompanies();
+		Class<?> clazz = getClass();
 
-		for (Company company : companies) {
-			ServiceContext serviceContext = new ServiceContext();
+		ClassLoader classLoader = clazz.getClassLoader();
 
-			serviceContext.setCompanyId(company.getCompanyId());
+		InputStream inputStream = classLoader.getResourceAsStream(
+			"definitions/r6j9-workflow-definition.xml");
 
-			int kaleoDefinitionsCount =
-				_kaleoDefinitionLocalService.getKaleoDefinitionsCount(
-					"R6J9 Conitional Approver", serviceContext);
+		String localizedTitle = LocalizationUtil.updateLocalization(
+			StringPool.BLANK, "title", "R6J9 Conditional Approver",
+			LocaleUtil.toLanguageId(LocaleUtil.getDefault()));
 
-			if (kaleoDefinitionsCount > 0) {
-				System.out.println("Already have the definition");
+		long companyId = _portal.getDefaultCompanyId();
 
-				return;
-			}
+		User defaultUser = _userLocalService.getDefaultUser(companyId);
 
-			Class<?> clazz = getClass();
-
-			ClassLoader classLoader = clazz.getClassLoader();
-
-			InputStream inputStream = classLoader.getResourceAsStream(
-				"definitions/r6j9-workflow-definition.xml");
-
-			if (inputStream == null) {
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Unable to find file for R6J9 Conditional Approver " +
-							"with file name r6j9-workflow-definition.xml");
-				}
-
-				return;
-			}
-
-			User defaultUser = _userLocalService.getDefaultUser(
-				company.getCompanyId());
-
-			String localizedTitle = LocalizationUtil.updateLocalization(
-				StringPool.BLANK, "title", "R6J9 Conditional Approver",
-				LocaleUtil.toLanguageId(LocaleUtil.getDefault()));
-
-			_workflowDefinitionManager.deployWorkflowDefinition(
-				serviceContext.getCompanyId(), defaultUser.getUserId(),
-				localizedTitle, "R6J9 Conditional Approver",
-				FileUtil.getBytes(inputStream));
-		}
+		_workflowDefinitionManager.deployWorkflowDefinition(
+			companyId, defaultUser.getUserId(), localizedTitle,
+			"R6J9 Conditional Approver", FileUtil.getBytes(inputStream));
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		R6J9ConditionEvaluator.class);
-
-	@Reference
-	private CompanyLocalService _companyLocalService;
 
 	@Reference
 	private KaleoDefinitionLocalService _kaleoDefinitionLocalService;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference
 	private UserLocalService _userLocalService;
