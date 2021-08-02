4837e1d2bc8c699c13d620a2e71111e4110ba5cf
jhinkey <james.hinkey@liferay.com>
2021-08-02 15:49:03 -0400

    LRDOCS-9606 Use single quotes to avoid character quoting. Include web content value.

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PATCH_ById.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PATCH_ById.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X PATCH \
 	"http://localhost:8080/o/headless-delivery/v1.0/structured-contents/${1}" \
-	-d "{\"friendlyUrlPath\": \"my-new-web-friendly-url\"}" \
+	-d '{"contentFields": [{"contentFieldValue": {"data": "<p>Bar</p>"}, "dataType": "string",  "name": "content"}], "friendlyUrlPath": "my-new-web-friendly-url"}' \
 	-u "test@liferay.com:test"
