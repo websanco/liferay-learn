abf9b025e6a9133801d9998a62e23e2ab6cbde2e
jhinkey <james.hinkey@liferay.com>
2021-08-02 15:51:08 -0400

    LRDOCS-9606 Remove script; consider adding in advanced tutorial.

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PUT_ByExternalReference.sh
+++
@@ -1,6 +0,0 @@
-curl \
-	-H "Content-Type: application/json" \
-	-X PUT \
-	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/structured-contents/by-external-reference-code/${2}" \
-	-d "{\"contentStructureId\": \"${3}\", \"title\": \"Updated Web Content article!\"}" \
-	-u "test@liferay.com:test"
