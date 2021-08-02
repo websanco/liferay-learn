79b84c5d614e248dcd058a723111647ccb9f670c
jhinkey <james.hinkey@liferay.com>
2021-08-02 14:46:29 -0400

    LRDOCS-9606 Content-Type: application/json is implied on GET commands. Remove indent and empty line.

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContents_GET_FromSite.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContents_GET_FromSite.sh
@@ -1,4 +1,3 @@
 curl \
-        -H "Content-Type: application/json" \
-		"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/structured-contents" \
-		-u "test@liferay.com:test"
+	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/structured-contents" \
+	-u "test@liferay.com:test"
