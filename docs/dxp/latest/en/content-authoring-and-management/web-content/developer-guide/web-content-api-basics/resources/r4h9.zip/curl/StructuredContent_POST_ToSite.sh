28cf6dc68da34bbf2f1279a2f78fb71b1b731a09
jhinkey <james.hinkey@liferay.com>
2021-08-03 11:56:53 -0400

    LRDOCS-9606 Use title and friendly URL consistently

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_POST_ToSite.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_POST_ToSite.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/structured-contents" \
-	-d "{\"contentFields\": [{\"contentFieldValue\": {\"data\": \"<p>Foo</p>\"}, \"dataType\": \"string\", \"name\": \"content\"}], \"contentStructureId\": \"${2}\", \"title\": \"Able\"}" \
+	-d "{\"contentFields\": [{\"contentFieldValue\": {\"data\": \"<p>Foo</p>\"}, \"dataType\": \"string\", \"name\": \"content\"}], \"contentStructureId\": \"${2}\", \"friendlyUrlPath\": \"able\", \"title\": \"Able\"}" \
 	-u "test@liferay.com:test"
