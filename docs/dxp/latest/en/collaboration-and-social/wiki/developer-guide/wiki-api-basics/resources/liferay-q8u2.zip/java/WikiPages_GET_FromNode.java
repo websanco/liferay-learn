5392a3314f22a53869d7d0fd473442cb72b99120
jhinkey <james.hinkey@liferay.com>
2021-08-04 17:05:06 -0400

    LRDOCS-9614 Rename to _GET_FromNode and _GET_FromParent

--- docs/dxp/latest/en/collaboration-and-social/wiki/developer-guide/wiki-api-basics/resources/liferay-q8u2.zip/java/WikiPages_GET_FromNode.java
+++ docs/dxp/latest/en/collaboration-and-social/wiki/developer-guide/wiki-api-basics/resources/liferay-q8u2.zip/java/WikiPages_GET_FromNode.java
@@ -3,10 +3,10 @@ import com.liferay.headless.delivery.client.pagination.Page;
 import com.liferay.headless.delivery.client.pagination.Pagination;
 import com.liferay.headless.delivery.client.resource.v1_0.WikiPageResource;
 
-public class WikiPages_GET_ByNodeId {
+public class WikiPages_GET_FromNode {
 
 	/**
-	 * java -classpath .:* -DwikiNodeId=1234 WikiPages_GET_ByNodeId
+	 * java -classpath .:* -DwikiNodeId=1234 WikiPages_GET_FromNode
 	 */
 	public static void main(String[] args) throws Exception {
 		WikiPageResource.Builder builder = WikiPageResource.builder();
--- docs/dxp/latest/en/collaboration-and-social/wiki/developer-guide/wiki-api-basics/resources/liferay-q8u2.zip/java/WikiPages_GET_FromParent.java
+++ docs/dxp/latest/en/collaboration-and-social/wiki/developer-guide/wiki-api-basics/resources/liferay-q8u2.zip/java/WikiPages_GET_FromParent.java
@@ -2,10 +2,10 @@ import com.liferay.headless.delivery.client.dto.v1_0.WikiPage;
 import com.liferay.headless.delivery.client.pagination.Page;
 import com.liferay.headless.delivery.client.resource.v1_0.WikiPageResource;
 
-public class WikiPages_GET_ByParentId {
+public class WikiPages_GET_FromParent {
 
 	/**
-	 * java -classpath .:* -DparentWikiPageId=1234 WikiPages_GET_ByParentId
+	 * java -classpath .:* -DparentWikiPageId=1234 WikiPages_GET_FromParent
 	 */
 	public static void main(String[] args) throws Exception {
 		WikiPageResource.Builder builder = WikiPageResource.builder();
