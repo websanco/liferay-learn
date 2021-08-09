affb178e02663324c87ea699c0f0cffe49c2323e
JR Houn <jr.houn@liferay.com>
2021-08-09 15:16:46 -0700

    Revert Workspace token because of instability with ga3

--- _common.sh
+++ _common.sh
@@ -12,7 +12,7 @@ readonly LIFERAY_LEARN_PORTAL_DOCKER_IMAGE_TOKEN=\\[\$LIFERAY_LEARN_PORTAL_DOCKE
 readonly LIFERAY_LEARN_PORTAL_DOCKER_IMAGE_VALUE=liferay\\/portal\\:7.4.2-ga3
 readonly LIFERAY_LEARN_PORTAL_GIT_TAG_TOKEN=\\[\$LIFERAY_LEARN_PORTAL_GIT_TAG\$\\]
 readonly LIFERAY_LEARN_PORTAL_GIT_TAG_VALUE=7.4.2-ga3
-readonly LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN=portal-7.4-ga3
+readonly LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN=portal-7.4-ga2
 
 function download_nexus_jar {
 	curl -L "https://repository-cdn.liferay.com/nexus/service/local/artifact/maven/redirect?a=${1}&g=com.liferay&r=liferay-public-releases&v=LATEST" -o $(ls -d liferay*.zip | head -n1)/java/${1}.jar
