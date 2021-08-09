b05e7c38254066e4df3c1c0dee8791bdc85c6632
jhinkey <james.hinkey@liferay.com>
2021-08-09 17:50:41 -0400

    LRDOCS-8985 MVC Render Command code

---
+++ docs/dxp/latest/en/developing-applications/developing-a-java-web-application/using-mvc/mvc-render-command/resources/liferay-a4p1.zip/a4p1-web/src/main/java/com/acme/a4p1/web/internal/portlet/A4P1Portlet.java
@@ -0,0 +1,20 @@
+package com.acme.a4p1.web.internal.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"javax.portlet.display-name=A4P1 Portlet",
+		"javax.portlet.init-param.view-template=/a4p1/foo.jsp",
+		"javax.portlet.name=com_acme_a4p1_web_internal_portlet_A4P1Portlet"
+	},
+	service = Portlet.class
+)
+public class A4P1Portlet extends MVCPortlet {
+}
---
+++ docs/dxp/latest/en/developing-applications/developing-a-java-web-application/using-mvc/mvc-render-command/resources/liferay-a4p1.zip/a4p1-web/src/main/java/com/acme/a4p1/web/internal/portlet/action/A4P1BarMVCRenderCommand.java
@@ -0,0 +1,35 @@
+package com.acme.a4p1.web.internal.portlet.action;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+	property = {
+		"javax.portlet.name=com_acme_a4p1_web_internal_portlet_A4P1Portlet",
+		"mvc.command.name=/a4p1/bar"
+	},
+	service = MVCRenderCommand.class
+)
+public class A4P1BarMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Invoke #render(RenderRequest, RenderResponse)");
+		}
+
+		return "/a4p1/bar.jsp";
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		A4P1BarMVCRenderCommand.class);
+
+}
