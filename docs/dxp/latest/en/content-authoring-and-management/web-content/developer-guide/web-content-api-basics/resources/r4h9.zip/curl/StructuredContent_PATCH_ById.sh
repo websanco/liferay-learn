632f995b241c5eb0a583a6bae554ffcd9bad24ec
Jorge Campo <62282406+jorge-campo@users.noreply.github.com>
2021-08-11 17:08:23 +0200

    LRDOCS-9606 cURL scripts --- remove dataType references

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PATCH_ById.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PATCH_ById.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X PATCH \
 	"http://localhost:8080/o/headless-delivery/v1.0/structured-contents/${1}" \
-	-d "{\"contentFields\": [{\"contentFieldValue\": {\"data\": \"<p>Bar</p>\"}, \"dataType\": \"string\", \"name\": \"content\"}]}" \
+	-d "{\"contentFields\": [{\"contentFieldValue\": {\"data\": \"<p>Bar</p>\"}, \"name\": \"content\"}]}" \
 	-u "test@liferay.com:test"
