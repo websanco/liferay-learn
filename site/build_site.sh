4ff9a24290ea02802a12e44503bdda5f1c26d7df
Rich Sezov <richard.sezov@liferay.com>
2021-08-13 13:31:03 -0400

    LRDOCS-9537 Turn Reference into Other; fix extra .zip in script

--- site/build_site.sh
+++ site/build_site.sh
@@ -261,7 +261,7 @@ function unzip_reference_docs {
 	# liferay-ce-portal-doc-*.zip
 	#
 
-	curl -L https://github.com/liferay/liferay-portal/releases/download/"${LIFERAY_LEARN_PORTAL_GIT_TAG_VALUE}"/"${LIFERAY_LEARN_PORTAL_DOC_FILE_NAME}".zip > liferay-ce-portal-doc.zip
+	curl -L https://github.com/liferay/liferay-portal/releases/download/"${LIFERAY_LEARN_PORTAL_GIT_TAG_VALUE}"/"${LIFERAY_LEARN_PORTAL_DOC_FILE_NAME}" > liferay-ce-portal-doc.zip
 
 	7z x liferay-ce-portal-doc.zip
 
