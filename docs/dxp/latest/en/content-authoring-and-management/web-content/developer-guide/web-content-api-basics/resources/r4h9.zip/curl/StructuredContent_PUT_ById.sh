ce91f550946a424502b9815100ded2ab374961c1
jhinkey <james.hinkey@liferay.com>
2021-08-16 09:10:18 -0400

    LRDOCS-9606 In PUT, demonstrate changing main attribute (e.g., title in this case)

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PUT_ById.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/web-content-api-basics/resources/r4h9.zip/curl/StructuredContent_PUT_ById.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X PUT \
 	"http://localhost:8080/o/headless-delivery/v1.0/structured-contents/${1}" \
-	-d "{\"contentFields\": [{\"contentFieldValue\": {\"data\": \"<p>Goo</p>\"}, \"name\": \"content\"}], \"contentStructureId\": \"${2}\", \"title\": \"Able\"}" \
+	-d "{\"contentFields\": [{\"contentFieldValue\": {\"data\": \"<p>Goo</p>\"}, \"name\": \"content\"}], \"contentStructureId\": \"${2}\", \"title\": \"Baker Article\"}" \
 	-u "test@liferay.com:test"
