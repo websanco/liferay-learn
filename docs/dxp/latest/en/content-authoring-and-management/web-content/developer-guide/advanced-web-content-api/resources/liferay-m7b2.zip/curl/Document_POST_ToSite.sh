511ed5b55efca7427096d53585abce4871332030
jhinkey <james.hinkey@liferay.com>
2021-08-30 15:01:40 -0400

    LRDOCS-9649 use liferay-m7b2.zip curl folder. Rename scripts.

--- docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/advanced-web-content-api/resources/liferay-m7b2.zip/curl/Document_POST_ToSite.sh
+++ docs/dxp/latest/en/content-authoring-and-management/web-content/developer-guide/advanced-web-content-api/resources/liferay-m7b2.zip/curl/Document_POST_ToSite.sh
@@ -1,5 +1,5 @@
 curl \
-	-F "file=@Foo.png" \
+	-F "file=@../Foo.png" \
 	-H "Content-Type: multipart/form-data" \
 	-X POST \
 	"http://localhost:8080/o/headless-delivery/v1.0/sites/${1}/documents" \
