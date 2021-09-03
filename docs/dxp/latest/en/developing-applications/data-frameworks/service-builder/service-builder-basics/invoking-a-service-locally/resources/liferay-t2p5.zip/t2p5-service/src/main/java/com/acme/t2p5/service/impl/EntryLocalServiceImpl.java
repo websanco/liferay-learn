20fc8e0df76161ea95d304aeab6c6ea239025b47
jhinkey <james.hinkey@liferay.com>
2021-09-03 13:21:12 -0400

    LRDOCS-9719 addEntry in the LSI

--- docs/dxp/latest/en/developing-applications/data-frameworks/service-builder/service-builder-basics/invoking-a-service-locally/resources/liferay-t2p5.zip/t2p5-service/src/main/java/com/acme/t2p5/service/impl/EntryLocalServiceImpl.java
+++ docs/dxp/latest/en/developing-applications/data-frameworks/service-builder/service-builder-basics/invoking-a-service-locally/resources/liferay-t2p5.zip/t2p5-service/src/main/java/com/acme/t2p5/service/impl/EntryLocalServiceImpl.java
@@ -1,31 +1,28 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
-
 package com.acme.t2p5.service.impl;
 
+import com.acme.t2p5.model.Entry;
 import com.acme.t2p5.service.base.EntryLocalServiceBaseImpl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
 
 import org.osgi.service.component.annotations.Component;
 
-/**
- * @author Brian Wing Shun Chan
- */
 @Component(
 	property = "model.class.name=com.acme.t2p5.model.Entry",
 	service = AopService.class
 )
 public class EntryLocalServiceImpl extends EntryLocalServiceBaseImpl {
+
+	public Entry addEntry(String name, String description)
+		throws PortalException {
+
+		Entry entry = createEntry(counterLocalService.increment());
+
+		entry.setName(name);
+		entry.setDescription(description);
+
+		return addEntry(entry);
+	}
+
 }
