56b1abbb52398e21a985d4dea6a3fc33dd1a96ce
jhinkey <james.hinkey@liferay.com>
2021-10-07 13:22:54 -0400

    LRDOCS-9070 follow blogs LSI's addEntry and updateAsset examples

--- docs/dxp/latest/en/developing-applications/data-frameworks/asset-framework/enabling-assets/resources/liferay-s5e6.zip/s5e6-service/src/main/java/com/acme/s5e6/service/impl/S5E6EntryLocalServiceImpl.java
+++ docs/dxp/latest/en/developing-applications/data-frameworks/asset-framework/enabling-assets/resources/liferay-s5e6.zip/s5e6-service/src/main/java/com/acme/s5e6/service/impl/S5E6EntryLocalServiceImpl.java
@@ -19,14 +19,15 @@ import com.acme.s5e6.service.base.S5E6EntryLocalServiceBaseImpl;
 
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.ContentTypes;
 
-import java.util.Date;
-
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -39,33 +40,38 @@ public class S5E6EntryLocalServiceImpl extends S5E6EntryLocalServiceBaseImpl {
 
 	@Indexable(type = IndexableType.REINDEX)
 	public S5E6Entry addS5E6Entry(
-			long companyId, long groupId, long userId, String userName,
-			String item, ServiceContext serviceContext)
+			long userId, long groupId, String description, String item,
+			ServiceContext serviceContext)
 		throws PortalException {
+
+		User user = _userLocalService.getUser(userId);
 
 		S5E6Entry s5e6Entry = s5e6EntryPersistence.create(
 			counterLocalService.increment());
 
-		Date now = new Date();
-
-		s5e6Entry.setCompanyId(companyId);
-		s5e6Entry.setCreateDate(serviceContext.getCreateDate(now));
+		s5e6Entry.setUuid(serviceContext.getUuid());
 		s5e6Entry.setGroupId(groupId);
-		s5e6Entry.setModifiedDate(serviceContext.getModifiedDate(now));
+		s5e6Entry.setCompanyId(user.getCompanyId());
+		s5e6Entry.setUserId(user.getUserId());
+		s5e6Entry.setUserName(user.getFullName());
+		s5e6Entry.setDescription(description);
 		s5e6Entry.setName(item);
-		s5e6Entry.setUserId(userId);
-		s5e6Entry.setUserName(userName);
 
 		s5e6Entry = s5e6EntryPersistence.update(s5e6Entry);
 
 		assetEntryLocalService.updateEntry(
-			userId, groupId, s5e6Entry.getCreateDate(),
-			s5e6Entry.getModifiedDate(), S5E6Entry.class.getName(),
-			s5e6Entry.getS5E6EntryId(), s5e6Entry.getUuid(), 0, null, null,
-			true, true, null, null, null, null, ContentTypes.TEXT,
-			s5e6Entry.getName(), null, null, null, null, 0, 0, 1.0);
+			s5e6Entry.getUserId(), s5e6Entry.getGroupId(),
+			s5e6Entry.getCreateDate(), s5e6Entry.getModifiedDate(),
+			S5E6Entry.class.getName(), s5e6Entry.getS5E6EntryId(),
+			s5e6Entry.getUuid(), 0, null, null, true, true, null, null, null,
+			null, ContentTypes.TEXT, s5e6Entry.getName(),
+			s5e6Entry.getDescription(), s5e6Entry.getDescription(), null, null,
+			0, 0, 1.0);
 
 		return s5e6Entry;
 	}
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
