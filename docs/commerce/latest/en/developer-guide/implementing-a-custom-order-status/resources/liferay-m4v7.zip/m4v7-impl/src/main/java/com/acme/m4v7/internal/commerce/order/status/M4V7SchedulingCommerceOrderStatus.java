b72f23e0fdc52917d97977770ba0960e5b7620a7
Aswin Rajeev <aswin.rajeev@liferay.com>
2021-11-01 14:03:22 +0000

    LRDOCS-7746 Code initial commit

---
+++ docs/commerce/latest/en/developer-guide/implementing-a-custom-order-status/resources/liferay-m4v7.zip/m4v7-impl/src/main/java/com/acme/m4v7/internal/commerce/order/status/M4V7SchedulingCommerceOrderStatus.java
@@ -0,0 +1,125 @@
+package com.acme.m4v7.internal.commerce.order.status;
+
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.order.status.CommerceOrderStatus;
+import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+@Component(
+	property = {
+		"commerce.order.status.key=" + M4V7SchedulingCommerceOrderStatus.KEY,
+		"commerce.order.status.priority:Integer=" + M4V7SchedulingCommerceOrderStatus.PRIORITY
+	},
+	service = CommerceOrderStatus.class
+)
+public class M4V7SchedulingCommerceOrderStatus implements CommerceOrderStatus {
+
+	public static final int KEY = 99;
+
+	public static final int PRIORITY = 40;
+
+	public static List<String> asList(Object object) {
+		if (object instanceof String) {
+			return new ArrayList<>(Collections.singletonList((String)object));
+		}
+		else if (object instanceof String[]) {
+			return new ArrayList<>(Arrays.asList((String[])object));
+		}
+		else if (object instanceof Collection) {
+			Collection<?> collection = (Collection<?>)object;
+
+			if (!collection.isEmpty()) {
+				Iterator<?> iterator = collection.iterator();
+
+				Object element = iterator.next();
+
+				if (element instanceof String) {
+					return new ArrayList<>((Collection<String>)object);
+				}
+			}
+		}
+
+		return new ArrayList<>();
+	}
+
+	@Override
+	public CommerceOrder doTransition(CommerceOrder commerceOrder, long userId)
+		throws PortalException {
+
+		commerceOrder.setOrderStatus(KEY);
+
+		return _commerceOrderService.updateCommerceOrder(commerceOrder);
+	}
+
+	@Override
+	public int getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "Scheduling");
+	}
+
+	@Override
+	public int getPriority() {
+		return PRIORITY;
+	}
+
+	@Override
+	public boolean isComplete(CommerceOrder commerceOrder) {
+		ExpandoBridge expandoBridge = commerceOrder.getExpandoBridge();
+
+		Object attributeValueObject = GetterUtil.getObject(
+			expandoBridge.getAttribute("Scheduling"));
+
+		List<String> attributeValueList = asList(attributeValueObject);
+
+		String schedulingStatus =
+			attributeValueList.isEmpty() ? "Pending" :
+				attributeValueList.get(0);
+
+		if (schedulingStatus.equalsIgnoreCase("Confirmed")) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean isTransitionCriteriaMet(CommerceOrder commerceOrder)
+		throws PortalException {
+
+		if (commerceOrder.getOrderStatus() ==
+				CommerceOrderConstants.ORDER_STATUS_PENDING) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	private volatile CommerceOrderService _commerceOrderService;
+
+}
