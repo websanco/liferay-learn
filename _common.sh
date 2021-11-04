0b2ae2c45110a9564c7ebc61ab81867ac979e8ce
jhinkey <james.hinkey@liferay.com>
2021-11-04 18:59:59 -0400

    Update Portal to 7.4 GA4

--- _common.sh
+++ _common.sh
@@ -8,13 +8,13 @@ readonly LIFERAY_LEARN_COMMERCE_WORKSPACE_TOKEN=
 readonly LIFERAY_LEARN_DXP_DOCKER_IMAGE_TOKEN=\\[\$LIFERAY_LEARN_DXP_DOCKER_IMAGE\$\\]
 readonly LIFERAY_LEARN_DXP_DOCKER_IMAGE_VALUE=liferay\\/dxp\\:7.3.10-dxp-2
 readonly LIFERAY_LEARN_DXP_WORKSPACE_TOKEN=dxp-7.3-sp2
-readonly LIFERAY_LEARN_PORTAL_DOC_FILE_NAME=liferay-ce-portal-doc-7.4.1-ga2-20210609223456272.zip
+readonly LIFERAY_LEARN_PORTAL_DOC_FILE_NAME=liferay-ce-portal-doc-7.4.3.4-ga4-20211020095956970.zip
 readonly LIFERAY_LEARN_PORTAL_DOCKER_IMAGE_TOKEN=\\[\$LIFERAY_LEARN_PORTAL_DOCKER_IMAGE\$\\]
-readonly LIFERAY_LEARN_PORTAL_DOCKER_IMAGE_VALUE=liferay\\/portal\\:7.4.1-ga2
+readonly LIFERAY_LEARN_PORTAL_DOCKER_IMAGE_VALUE=liferay\\/portal\\:7.4.3.4-ga4
 readonly LIFERAY_LEARN_PORTAL_GIT_TAG_TOKEN=\\[\$LIFERAY_LEARN_PORTAL_GIT_TAG\$\\]
-readonly LIFERAY_LEARN_PORTAL_GIT_TAG_VALUE=7.4.1-ga2
+readonly LIFERAY_LEARN_PORTAL_GIT_TAG_VALUE=7.4.3.4-ga4
 readonly LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN=\\[\$LIFERAY_LEARN_PORTAL_WORKSPACE\$\\]
-readonly LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN_VALUE=portal-7.4-ga2
+readonly LIFERAY_LEARN_PORTAL_WORKSPACE_TOKEN_VALUE=portal-7.4-ga4
 
 function download_nexus_jar {
 	curl -L "https://repository-cdn.liferay.com/nexus/service/local/artifact/maven/redirect?a=${1}&g=com.liferay&r=liferay-public-releases&v=LATEST" -o $(ls -d liferay*.zip | head -n1)/java/${1}.jar
