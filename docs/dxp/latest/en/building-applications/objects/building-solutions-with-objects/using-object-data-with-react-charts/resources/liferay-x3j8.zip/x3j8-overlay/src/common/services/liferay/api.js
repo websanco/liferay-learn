66d3b9a505208e640fef875248c03a937e982f66
JamesG <James.Garcia@Liferay.com>
2022-01-14 20:25:26 -0800

    LRDOCS-9987 removes auth token from api.js

--- docs/dxp/latest/en/building-applications/objects/building-solutions-with-objects/using-object-data-with-react-charts/resources/liferay-x3j8.zip/x3j8-overlay/src/common/services/liferay/api.js
+++ docs/dxp/latest/en/building-applications/objects/building-solutions-with-objects/using-object-data-with-react-charts/resources/liferay-x3j8.zip/x3j8-overlay/src/common/services/liferay/api.js
@@ -1,45 +1,24 @@
-const {REACT_APP_LIFERAY_API = window.location.origin} = process.env;
+const { REACT_APP_LIFERAY_API = window.location.origin } = process.env
 
-export const getLiferayAuthenticationToken = () => {
-	try {
-		// eslint-disable-next-line no-undef
-		const token = Liferay.authToken;
+const baseFetch = async (url, { body, method = 'GET' } = {}) => {
+  let headers = new Headers({
+    Authorization: 'Basic ' + btoa('test@liferay.com:test'),
+    'Content-Type': 'application/json'
+  })
 
-		return token;
-	} catch (error) {
-		console.warn('Not able to find Liferay auth token\n', error);
+  let apiPath = 'http://localhost:8080'
 
-		return '';
-	}
-};
+  const response = await fetch(apiPath + '/' + url, {
+    ...(body && { body: JSON.stringify(body) }),
+    headers: headers,
+    method
+  })
 
-const baseFetch = async (url, {body, method = 'GET'} = {}) => {
-	let headers = new Headers({
-		'Content-Type': 'application/json',
-		'x-csrf-token': getLiferayAuthenticationToken(),
-	});
+  const data = await response.json()
 
-	let apiPath = REACT_APP_LIFERAY_API;
+  return { data }
+}
 
-	if (getLiferayAuthenticationToken() === '') {
-		headers = new Headers({
-			Authorization: 'Basic ' + btoa('test@liferay.com:test'),
-			'Content-Type': 'application/json',
-		});
-		apiPath = 'http://localhost:8080';
-	}
+export { REACT_APP_LIFERAY_API }
 
-	const response = await fetch(apiPath + '/' + url, {
-		...(body && {body: JSON.stringify(body)}),
-		headers: headers,
-		method,
-	});
-
-	const data = await response.json();
-
-	return {data};
-};
-
-export {REACT_APP_LIFERAY_API};
-
-export default baseFetch;
+export default baseFetch
