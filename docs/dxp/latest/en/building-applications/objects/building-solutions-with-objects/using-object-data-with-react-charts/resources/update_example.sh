2071d44bd49306beaf67591ea5a5676876f1a465
JamesG <James.Garcia@Liferay.com>
2022-01-21 12:57:42 -0800

    LRDOCS-9987 adds rm command, simplifies cp and rm steps, sorts package.json

--- docs/dxp/latest/en/building-applications/objects/building-solutions-with-objects/using-object-data-with-react-charts/resources/update_example.sh
+++ docs/dxp/latest/en/building-applications/objects/building-solutions-with-objects/using-object-data-with-react-charts/resources/update_example.sh
@@ -5,21 +5,21 @@ readonly CURRENT_DIR_NAME=$(dirname "$0")
 function generate_remote_app {
 	cd liferay-x3j8.zip
 
+	rm -fr x3j8-remote-app
+
 	curl -Ls https://github.com/liferay/liferay-portal/raw/master/tools/create_remote_app.sh | bash -s x3j8-remote-app react
 
 	jq -s '.[0] * .[1]' ./x3j8-overlay/package.json ./x3j8-remote-app/package.json > package.json
 
 	mv package.json ./x3j8-remote-app/package.json
 
-	rm -R x3j8-remote-app/src
+	rm -fr ./x3j8-remote-app/src
 
-	cd x3j8-overlay
+	cp -r ./x3j8-overlay/src x3j8-remote-app
 
-	cp -R src ../x3j8-remote-app
+	rm -fr x3j8-overlay
 
-	cd ../x3j8-remote-app
-	
-	rm -R ../x3j8-overlay
+	cd x3j8-remote-app
 
 	yarn install
 }
