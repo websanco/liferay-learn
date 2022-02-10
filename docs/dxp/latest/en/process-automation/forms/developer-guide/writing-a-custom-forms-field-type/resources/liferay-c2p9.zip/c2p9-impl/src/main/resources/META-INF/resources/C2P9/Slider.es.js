e1bdd38ad70a496e7093ef5a68f83d3f48489d9e
Russell Bohl <russell.bohl@liferay.com>
2022-02-10 12:02:35 -0500

    LRDOCS-9335 formatting: spaces > tabs

--- docs/dxp/latest/en/process-automation/forms/developer-guide/writing-a-custom-forms-field-type/resources/liferay-c2p9.zip/c2p9-impl/src/main/resources/META-INF/resources/C2P9/Slider.es.js
+++ docs/dxp/latest/en/process-automation/forms/developer-guide/writing-a-custom-forms-field-type/resources/liferay-c2p9.zip/c2p9-impl/src/main/resources/META-INF/resources/C2P9/Slider.es.js
@@ -7,40 +7,40 @@ import React from 'react';
  */
 
 const Slider = ({readOnly, name, onChange, predefinedValue, value}) => 
-    <input 
-        className="ddm-field-slider form-control slider"
-        onInput={onChange}
-        disabled={readOnly}
-        id="myRange"
-        max={100}
-        min={1}
-        name={name}
-        type="range"
-        value={value ? value : predefinedValue}/>
+	<input 
+		className="ddm-field-slider form-control slider"
+		onInput={onChange}
+		disabled={readOnly}
+		id="myRange"
+		max={100}
+		min={1}
+		name={name}
+		type="range"
+		value={value ? value : predefinedValue}/>
 
 const Main = ({label, name, onChange, predefinedValue, readOnly, value, ...otherProps}) =>{
 
-    const [currentValue, setCurrentValue] = useSyncValue(
+	const [currentValue, setCurrentValue] = useSyncValue(
 		value ? value : predefinedValue
 	);
 
-    return <FieldBase
+	return <FieldBase
 			label={label}
-            name={name}
-            predefinedValue={predefinedValue}
+			name={name}
+			predefinedValue={predefinedValue}
 			{...otherProps}
 		>
-        <Slider 
-            name={name}
-            onChange={(event) => {
-                setCurrentValue(event.target.value);
-                onChange(event);
-            }}
-            predefinedValue={predefinedValue}
-            readOnly={readOnly}
-            value={currentValue}
-        />
-    </FieldBase>
+		<Slider 
+			name={name}
+			onChange={(event) => {
+				setCurrentValue(event.target.value);
+				onChange(event);
+			}}
+			predefinedValue={predefinedValue}
+			readOnly={readOnly}
+			value={currentValue}
+		/>
+	</FieldBase>
 }
 
 Main.displayName = 'Slider';
