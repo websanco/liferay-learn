c17b87c801cc21fc8d4da61ade791b33e72f1263
JamesG <james.garcia@liferay.com>
2022-04-11 18:34:08 -0700

    LRDOCS-10295 adds cURL scripts for Nested Fields article

---
+++ docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/using-nested-fields-with-objects/resources/liferay-w4s7.zip/curl/AbleObject_POST_ToCompany.sh
@@ -0,0 +1,6 @@
+curl \
+	-H "Content-Type: application/json" \
+	-X POST \
+	"http://localhost:8080/o/c/ableobjects/" \
+	-d "{\"name\": \"Foo\"}" \
+	-u "test@liferay.com:test"
---
+++ docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/using-nested-fields-with-objects/resources/liferay-w4s7.zip/curl/CharlieObject_GET_ById.sh
@@ -0,0 +1,3 @@
+curl \
+	"http://localhost:8080/o/c/charlieobjects/${1}/?nestedFields=bakerObject,ableObject&nestedFieldsDepth=2" \
+	-u "test@liferay.com:test"
