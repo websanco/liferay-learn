d3084c389c7680afba919ad24449e692352638b6
Brian Chan <brian.chan@liferay.com>
2022-04-15 09:56:21 -0300

    LRDOCS-10002 Update 'Able 1' to 'Able One'

--- docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/object-api-basics/resources/liferay-v1s4.zip/curl/Able_PUT_ById.sh
+++ docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/object-api-basics/resources/liferay-v1s4.zip/curl/Able_PUT_ById.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X PUT \
 	"http://localhost:8080/o/c/ables/${1}" \
-	-d "{\"name\": \"Able Two\"}" \
+	-d "{\"name\": \"Able One\"}" \
 	-u "test@liferay.com:test"
