edd8e6092ce17f5cbb6c530dbbb2055602d82737
Brian Chan <brian.chan@liferay.com>
2022-04-15 09:53:52 -0300

    LRDOCS-10234 Use numbers

--- docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/batch-object-apis/resources/liferay-t4r3.zip/curl/Ables_POST_Batch.sh
+++ docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/batch-object-apis/resources/liferay-t4r3.zip/curl/Ables_POST_Batch.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X POST \
 	"http://localhost:8080/o/c/ables/batch" \
-	-d "[{\"name\": \"Able One\"}, {\"name\": \"Able Two\"}, {\"name\": \"Able Three\"}]" \
+	-d "[{\"name\": \"Able 1\"}, {\"name\": \"Able 2\"}, {\"name\": \"Able 3\"}]" \
 	-u "test@liferay.com:test"
--- docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/batch-object-apis/resources/liferay-t4r3.zip/curl/Ables_PUT_Batch.sh
+++ docs/dxp/latest/en/building-applications/objects/objects-tutorials/using-apis/batch-object-apis/resources/liferay-t4r3.zip/curl/Ables_PUT_Batch.sh
@@ -2,5 +2,5 @@ curl \
 	-H "Content-Type: application/json" \
 	-X PUT \
 	"http://localhost:8080/o/c/ables/batch" \
-	-d "[{\"id\": ${1}, \"name\": \"Able Four\"}, {\"id\": ${2}, \"name\": \"Able Five\"}, {\"id\": ${3}, \"name\": \"Able Six\"}]" \
+	-d "[{\"id\": ${1}, \"name\": \"Able 4\"}, {\"id\": ${2}, \"name\": \"Able 5\"}, {\"id\": ${3}, \"name\": \"Able 6\"}]" \
 	-u "test@liferay.com:test"
