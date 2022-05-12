4b4b69370e830f07e94d574f21e0a3275b015767
mtokudome <makoto.tokudome@liferay.com>
2022-05-12 13:41:46 -0700

    LRDOCS-10519 fix arguments

--- docs/dxp/latest/en/users-and-permissions/developer-guide/accounts-api-basics/resources/liferay-t5p9.zip/curl/AccountRole_POST_UserAssociation.sh
+++ docs/dxp/latest/en/users-and-permissions/developer-guide/accounts-api-basics/resources/liferay-t5p9.zip/curl/AccountRole_POST_UserAssociation.sh
@@ -1,5 +1,5 @@
 curl \
 	-H "Content-Type: application/json" \
 	-X POST \
-	"http://localhost:8080/o/headless-admin-user/v1.0/accounts/${1}/account-roles/${1}/user-accounts/${1}" \
+	"http://localhost:8080/o/headless-admin-user/v1.0/accounts/${1}/account-roles/${2}/user-accounts/${3}" \
 	-u "test@liferay.com:learn"
--- docs/dxp/latest/en/users-and-permissions/developer-guide/api/role-management-api/resources/liferay-z3v5.zip/curl/RoleUserAssociation_POST_ToInstance.sh
+++ docs/dxp/latest/en/users-and-permissions/developer-guide/api/role-management-api/resources/liferay-z3v5.zip/curl/RoleUserAssociation_POST_ToInstance.sh
@@ -1,5 +1,5 @@
 curl \
 	-H "Content-Type: application/json" \
 	-X POST \
-	"http://localhost:8080/o/headless-admin-user/v1.0/roles/${1}/association/user-account/${1}" \
+	"http://localhost:8080/o/headless-admin-user/v1.0/roles/${1}/association/user-account/${2}" \
 	-u "test@liferay.com:learn"
